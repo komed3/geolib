b2b776642f29642181861cf705dc860ffc3d73f3
komed3 <webmaster@komed3.de>
2026-09-01 23:47:34 +0200

    feat: create GeodeticMath.ts

---
+++ src/math/GeodeticMath.ts
@@ -0,0 +1,5 @@
+export class GeodeticMath {
+  public static readonly EARTH_RADIUS_METERS = 6371000;
+  public static readonly WEB_MERCATOR_RADIUS = 6378137;
+  public static readonly METERS_PER_DEGREE_LAT = 111_320;
+}
