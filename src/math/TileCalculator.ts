ae730e3d362c22b4416c2e88b733280a06fb081e
komed3 <webmaster@komed3.de>
2026-09-01 23:53:25 +0200

    convert lat/lng/zoom to tile

--- src/math/TileCalculator.ts
+++ src/math/TileCalculator.ts
@@ -1,5 +1,22 @@
+import { GeodeticMath } from './GeodeticMath';
+
+
 export interface TileXY {
   x: number;
   y: number;
   z: number;
+}
+
+
+export class TileCalculator {
+  public static toTile ( lat: number, lng: number, zoom: number ) : TileXY {
+    const latitude = Math.min( Math.max( lat, -85.05112878 ), 85.05112878 );
+    const sinLat = Math.sin( GeodeticMath.degToRad( latitude ) );
+
+    return {
+      x: Math.floor( ( ( lng + 180 ) / 360 ) * 2 ** zoom ),
+      y: Math.floor( ( 0.5 - Math.log( ( 1 + sinLat ) / ( 1 - sinLat ) ) / ( 4 * Math.PI ) ) * 2 ** zoom ),
+      z: zoom
+    };
+  }
 }
