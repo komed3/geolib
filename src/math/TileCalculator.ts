ae30eba45f4c8f60b496b9ea7913d9c3a697837b
komed3 <webmaster@komed3.de>
2026-09-01 23:56:07 +0200

    update TileCalculator.ts

--- src/math/TileCalculator.ts
+++ src/math/TileCalculator.ts
@@ -1,3 +1,4 @@
+import { GeographicCoordinate } from '../system/GeographicCoordinate';
 import { GeodeticMath } from './GeodeticMath';
 
 
@@ -9,14 +10,23 @@ export interface TileXY {
 
 
 export class TileCalculator {
-  public static toTile ( lat: number, lng: number, zoom: number ) : TileXY {
+  public static toTile ( lat: number, lon: number, zoom: number ) : TileXY {
     const latitude = Math.min( Math.max( lat, -85.05112878 ), 85.05112878 );
     const sinLat = Math.sin( GeodeticMath.degToRad( latitude ) );
 
     return {
-      x: Math.floor( ( ( lng + 180 ) / 360 ) * 2 ** zoom ),
+      x: Math.floor( ( ( lon + 180 ) / 360 ) * 2 ** zoom ),
       y: Math.floor( ( 0.5 - Math.log( ( 1 + sinLat ) / ( 1 - sinLat ) ) / ( 4 * Math.PI ) ) * 2 ** zoom ),
       z: zoom
     };
+  }
+
+  public static toLatLon ( x: number, y: number, zoom: number ) : GeographicCoordinate {
+    const n = 2 ** zoom;
+
+    const lon = ( x / n ) * 360 - 180;
+    const lat = GeodeticMath.radToDeg( Math.atan( Math.sinh( Math.PI * ( 1 - ( 2 * y ) / n ) ) ) );
+
+    return new GeographicCoordinate( lat, lon );
   }
 }
