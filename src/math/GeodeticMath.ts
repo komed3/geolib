a452cc3552739153adf93a1913a386f7e996fd6b
komed3 <webmaster@komed3.de>
2026-09-01 23:48:21 +0200

    update GeodeticMath.ts

--- src/math/GeodeticMath.ts
+++ src/math/GeodeticMath.ts
@@ -2,4 +2,12 @@ export class GeodeticMath {
   public static readonly EARTH_RADIUS_METERS = 6371000;
   public static readonly WEB_MERCATOR_RADIUS = 6378137;
   public static readonly METERS_PER_DEGREE_LAT = 111_320;
+
+  public static degToRad ( deg: number ) : number {
+    return ( deg * Math.PI ) / 180;
+  }
+
+  public static radToDeg ( rad: number ) : number {
+    return ( rad * 180 ) / Math.PI;
+  }
 }
