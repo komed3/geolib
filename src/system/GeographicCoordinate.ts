8e049418783eeaae4656494f7a41537acf0fbf84
komed3 <webmaster@komed3.de>
2026-09-02 09:32:54 +0200

    update GeographicCoordinate.ts

--- src/system/GeographicCoordinate.ts
+++ src/system/GeographicCoordinate.ts
@@ -11,13 +11,13 @@ export class GeographicCoordinate extends Coordinate {
     super( 'geographic', 3 );
 
     if ( ! Number.isFinite( latitude ) || ! Number.isFinite( longitude ) || ! Number.isFinite( altitude ) )
-      throw new TypeError( 'Latitude, longitude and altitude must be finite numbers.' );
+      throw new TypeError( 'latitude, longitude and altitude must be finite numbers' );
 
     if ( latitude < -90 || latitude > 90 )
-      throw new RangeError( 'Latitude must be between -90 and 90 degrees.' );
+      throw new RangeError( 'latitude must be between -90 and 90 degrees' );
 
     if ( longitude < -180 || longitude > 180 )
-      throw new RangeError( 'Longitude must be between -180 and 180 degrees.' );
+      throw new RangeError( 'longitude must be between -180 and 180 degrees' );
 
     this.latitude = latitude;
     this.longitude = longitude;
