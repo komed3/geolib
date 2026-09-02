9cb82fa9389be0b02341eedc8a7c4dac29086977
komed3 <webmaster@komed3.de>
2026-09-02 09:49:14 +0200

    update DistanceCalculator.ts

--- src/math/DistanceCalculator.ts
+++ src/math/DistanceCalculator.ts
@@ -10,8 +10,6 @@ export class DistanceCalculator {
     const dLon = GeodeticMath.degToRad( to.lon - from.lon );
 
     const a = Math.sin( dLat / 2 ) ** 2 + Math.cos( lat1 ) * Math.cos( lat2 ) * Math.sin( dLon / 2 ) ** 2;
-    const c = 2 * Math.atan2( Math.sqrt( a ), Math.sqrt( 1 - a ) );
-
-    return GeodeticMath.EARTH_RADIUS_METERS * c;
+    return GeodeticMath.EARTH_RADIUS_METERS * ( 2 * Math.atan2( Math.sqrt( a ), Math.sqrt( 1 - a ) ) );
   }
 }
