fa93b0b8c34de5b36e4e362f6ea57b63c8b8b1ac
komed3 <webmaster@komed3.de>
2026-09-02 09:38:07 +0200

    update MapScaleCalculator.ts

--- src/math/MapScaleCalculator.ts
+++ src/math/MapScaleCalculator.ts
@@ -4,5 +4,7 @@ import { GeodeticMath } from './GeodeticMath.js';
 export class MapScaleCalculator {
   public static metersPerPixel ( lat: number, zoom: number ) : number {}
 
-  public static scaleForZoom ( zoom: number, lat: number, pixelWidth = 256 ) : number {}
+  public static scaleForZoom ( zoom: number, lat: number, pixelWidth = 256 ) : number {
+    return ( this.metersPerPixel( lat, zoom ) * pixelWidth ) / 0.00028;
+  }
 }
