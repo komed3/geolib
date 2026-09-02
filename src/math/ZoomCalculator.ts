42d28d710e4eafc8c7b9d5e14627c3e3a5f99d27
komed3 <webmaster@komed3.de>
2026-09-02 09:31:43 +0200

    update ZoomCalculator.ts

--- src/math/ZoomCalculator.ts
+++ src/math/ZoomCalculator.ts
@@ -2,4 +2,11 @@ import { GeodeticMath } from './GeodeticMath.js';
 
 
 export class ZoomCalculator {
+  public static metersPerPixel ( meters: number, lat: number ) : number {
+    //
+  }
+
+  public static scale ( scale: number, lat: number) : number {
+    //
+  }
 }
