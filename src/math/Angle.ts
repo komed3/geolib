86280aa6945f7961db52361f08b1d8370198b3e0
komed3 <webmaster@komed3.de>
2026-09-02 09:48:46 +0200

    bearing

--- src/math/Angle.ts
+++ src/math/Angle.ts
@@ -11,5 +11,14 @@ export class Angle {
     return GeodeticMath.radToDeg( rad );
   }
 
-  public static bearing ( from: GeographicCoordinate, to: GeographicCoordinate ) : number {}
+  public static bearing ( from: GeographicCoordinate, to: GeographicCoordinate ) : number {
+    const lat1 = GeodeticMath.degToRad( from.lat );
+    const lat2 = GeodeticMath.degToRad( to.lat );
+    const dLon = GeodeticMath.degToRad( to.lon - from.lon );
+
+    return ( GeodeticMath.radToDeg(
+      Math.atan2( Math.sin( dLon ) * Math.cos( lat2 ),
+      Math.cos( lat1 ) * Math.sin( lat2 ) - Math.sin( lat1 ) * Math.cos( lat2 ) * Math.cos( dLon ) )
+    ) + 360 ) % 360;
+  }
 }
