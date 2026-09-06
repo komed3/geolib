8ed243953163e304515e0fcfd7f7a99a22188053
komed3 <webmaster@komed3.de>
2026-09-07 00:16:48 +0200

    update CRSRegistry.ts

--- src/registry/CRSRegistry.ts
+++ src/registry/CRSRegistry.ts
@@ -3,19 +3,7 @@ import { Registry } from './Registry';
 
 
 export class CRSRegistry extends Registry< CRS > {
-  public override register ( crs: CRS ) : void {
-    super.register( { ...crs, code: crs.code.toUpperCase() } as CRS );
-  }
-
-  public override get ( code: string ) : CRS | undefined {
-    return super.get( code.toUpperCase() );
-  }
-
-  public override has ( code: string ) : boolean {
-    return super.has( code.toUpperCase() );
-  }
-
-  public override remove ( code: string ) : boolean {
-    return super.remove( code.toUpperCase() );
+  protected override normalize ( code: string ) : string {
+    return code.toUpperCase();
   }
 }
