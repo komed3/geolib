452081213b38abd979b2f65bc4de32393e03d829
komed3 <webmaster@komed3.de>
2026-09-07 00:34:50 +0200

    update crs.ts

--- src/registry/crs.ts
+++ src/registry/crs.ts
@@ -1,4 +1,16 @@
+import { CoordinateSystem } from '../crs/CoordinateSystem';
 import { CRSRegistry } from './CRSRegistry';
+
+
+const geographic2D = new CoordinateSystem( 'ellipsoidal', [
+  { name: 'Geodetic latitude', direction: 'north', unit: 'degree' },
+  { name: 'Geodetic longitude', direction: 'east', unit: 'degree' }
+] );
+
+const projected2D = new CoordinateSystem( 'cartesian', [
+  { name: 'Easting', direction: 'east', unit: 'metre' },
+  { name: 'Northing', direction: 'north', unit: 'metre' }
+] );
 
 
 export const crs = new CRSRegistry();
