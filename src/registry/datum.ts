1aa7a1160d4efb05b0fb815f6ab2ef8c3f8a24de
komed3 <webmaster@komed3.de>
2026-09-07 00:31:34 +0200

    declare entries

--- src/registry/datum.ts
+++ src/registry/datum.ts
@@ -1,4 +1,10 @@
+import { Datum } from '../crs/Datum';
 import { DatumRegistry } from './DatumRegistry';
+import { GRS80, WGS84 } from './ellipsoids';
 
 
 export const datums = new DatumRegistry();
+
+export const WGS84_DATUM = new Datum( 'EPSG:6326', 'World Geodetic System 1984', WGS84 );
+export const ETRS89 = new Datum( 'EPSG:6258', 'European Terrestrial Reference System 1989', GRS80 );
+export const NAD83 = new Datum( 'EPSG:6269', 'North American Datum 1983', GRS80 );
