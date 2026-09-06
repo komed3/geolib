61b969fbb83609ff3a982876b62a56bd33f31c82
komed3 <webmaster@komed3.de>
2026-09-06 11:17:53 +0200

    update DMS.ts

--- src/coordinate/DMS.ts
+++ src/coordinate/DMS.ts
@@ -5,5 +5,11 @@ export class DMS {
   public constructor( degrees: number, minutes: number, seconds: number, dir: DMSDirection ) {
     if ( ! Number.isInteger( degrees ) || degrees < 0 )
       throw new RangeError( 'Degrees must be a non-negative integer' );
+
+    if ( ! Number.isInteger( minutes ) || minutes < 0 || minutes >= 60 )
+      throw new RangeError( 'Minutes must be between 0 and 59' );
+
+    if ( ! Number.isFinite( seconds ) || seconds < 0 || seconds >= 60 )
+      throw new RangeError( 'Seconds must be between 0 and 60' );
   }
 }
