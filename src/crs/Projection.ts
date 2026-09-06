db9fca5f80c82b04f4d3725979e55faf2d3469ea
komed3 <webmaster@komed3.de>
2026-09-06 23:50:44 +0200

    update Projection.ts

--- src/crs/Projection.ts
+++ src/crs/Projection.ts
@@ -5,10 +5,16 @@ export class Projection {
   public constructor (
     public readonly name: string,
     public readonly method: string,
-    public readonly parameters: ProjectionParams = {}
+    public readonly params: ProjectionParams = {}
   ) {}
 
   public clone () : Projection {
-    return new Projection( this.name, this.method, { ...this.parameters } );
+    return new Projection( this.name, this.method, { ...this.params } );
+  }
+
+  public equals ( other: Projection ) : boolean {
+    return this.name === other.name && this.method === other.method &&
+      Object.keys( this.params ).length === Object.keys( other.params ).length &&
+      Object.keys( this.params ).every( k => this.params[ k ] === other.params[ k ] );
   }
 }
