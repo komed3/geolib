716e241fb8591292e53bf6ca9d601a6e1d326038
komed3 <webmaster@komed3.de>
2026-09-06 19:13:26 +0200

    update Registry.ts

--- src/abstract/Registry.ts
+++ src/abstract/Registry.ts
@@ -1,3 +1,8 @@
 export abstract class Registry< T > {
   private readonly entries = new Map< string, T >();
+
+  public register ( key: string, value: T ) : void {
+    if ( this.entries.has( key ) ) throw new Error( `Entry already registered: ${ key }` );
+    this.entries.set( key, value );
+  }
 }
