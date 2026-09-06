5e0d596fb4fe804e41942f41a74aa970155ce21e
komed3 <webmaster@komed3.de>
2026-09-07 00:10:31 +0200

    update Registry.ts

--- src/crs/Registry.ts
+++ src/crs/Registry.ts
@@ -5,4 +5,20 @@ interface RegistryEntry {
 
 export abstract class Registry< T extends RegistryEntry > {
   protected readonly entries = new Map< string, T >();
+
+  public register ( entry: T ) : void {
+    this.entries.set( entry.code, entry );
+  }
+
+  public get ( code: string ) : T | undefined {
+    return this.entries.get( code );
+  }
+
+  public has ( code: string ) : boolean {
+    return this.entries.has( code );
+  }
+
+  public remove ( code: string ) : boolean {
+    return this.entries.delete( code );
+  }
 }
