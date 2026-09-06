e8c0a91b77206e9cd30e572f5b847cd690a221cd
komed3 <webmaster@komed3.de>
2026-09-06 10:43:11 +0200

    throw range error

--- src/coordinate/Latitude.ts
+++ src/coordinate/Latitude.ts
@@ -5,6 +5,9 @@ export class Latitude {
     if ( ! Number.isFinite( value ) )
       throw new TypeError( 'Latitude must be a finite number' );
 
+    if ( value < -90 || value > 90 )
+      throw new RangeError( 'Latitude must be between -90 and 90 degrees' );
+
     this.value = value;
   }
 }
