e06416403cb569f5f158f6d03bf96bed5d4e0d45
komed3 <webmaster@komed3.de>
2026-09-06 23:23:47 +0200

    declare types / interfaces

--- src/crs/CoordinateSystem.ts
+++ src/crs/CoordinateSystem.ts
@@ -1,1 +1,12 @@
+export type CoordinateSystemType = 'ellipsoidal' | 'cartesian';
+export type AxisDirection = 'north' | 'south' | 'east' | 'west' | 'up' | 'down';
+export type AxisUnit = | 'degree' | 'metre';
+
+export interface CoordinateAxis {
+  readonly name: string;
+  readonly direction: AxisDirection;
+  readonly unit: AxisUnit;
+}
+
+
 export class CoordinateSystem {}
