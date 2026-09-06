e4cb182444d7dc0235862ec29e0e6394b7c4548b
komed3 <webmaster@komed3.de>
2026-09-06 11:39:23 +0200

    update Coordinate.ts

--- src/coordinate/Coordinate.ts
+++ src/coordinate/Coordinate.ts
@@ -1,3 +1,4 @@
+import { DMS } from './DMS';
 import { Latitude } from './Latitude';
 import { Longitude } from './Longitude';
 
@@ -24,11 +25,25 @@ export class Coordinate {
     return [ this.latitude.toRadians(), this.longitude.toRadians() ];
   }
 
+  public toDMS () : [ DMS, DMS ] {
+    return [ DMS.fromLatitude( this.latitude.value ), DMS.fromLongitude( this.longitude.value ) ];
+  }
+
   public toString ( { delimiter, ...o }: StringOptions = {} ) : string {
     return [ this.latitude.toString( o ), this.longitude.toString( o ) ].join( delimiter ?? ';' );
   }
 
   public static fromDegrees ( latitude: number, longitude: number ) : Coordinate {
     return new Coordinate( new Latitude( latitude ), new Longitude( longitude ) );
+  }
+
+  public static fromDMS ( latitude: DMS, longitude: DMS ) : Coordinate {
+    if ( latitude.direction !== 'N' && latitude.direction !== 'S' )
+      throw new TypeError( 'Latitude must use N or S direction' );
+
+    if ( longitude.direction !== 'E' && longitude.direction !== 'W' )
+      throw new TypeError( 'Longitude must use E or W direction' );
+
+    return Coordinate.fromDegrees( latitude.toDecimal(), longitude.toDecimal() );
   }
 }
