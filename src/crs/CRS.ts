927700f5be6bdf4864cf10037c929dbccedc1152
komed3 <webmaster@komed3.de>
2026-09-06 19:14:53 +0200

    setup CRS

--- src/crs/CRS.ts
+++ src/crs/CRS.ts
@@ -4,4 +4,11 @@ import { Datum } from './Datum';
 export type CRSType = 'geographic' | 'projected';
 
 
-export class CRS {}
+export class CRS {
+  public constructor (
+    public readonly code: string,
+    public readonly name: string,
+    public readonly type: CRSType,
+    public readonly datum: Datum
+  ) {}
+}
