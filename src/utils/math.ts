aa2d507c120e18428a9c083feaa425685561e884
komed3 <webmaster@komed3.de>
2026-09-06 13:59:34 +0200

    feat: create math.ts

---
+++ src/utils/math.ts
@@ -0,0 +1,7 @@
+export function deg2Rad ( degrees: number ) : number {
+  return degrees * Math.PI / 180;
+}
+
+export function rad2Deg ( radians: number ) : number {
+  return radians * 180 / Math.PI;
+}
