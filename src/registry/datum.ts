f5638882108323b0cc9b79e11c0c9b1d5b974a79
komed3 <webmaster@komed3.de>
2026-09-07 00:31:44 +0200

    update datum.ts

--- src/registry/datum.ts
+++ src/registry/datum.ts
@@ -8,3 +8,7 @@ export const datums = new DatumRegistry();
 export const WGS84_DATUM = new Datum( 'EPSG:6326', 'World Geodetic System 1984', WGS84 );
 export const ETRS89 = new Datum( 'EPSG:6258', 'European Terrestrial Reference System 1989', GRS80 );
 export const NAD83 = new Datum( 'EPSG:6269', 'North American Datum 1983', GRS80 );
+
+datums.register( WGS84_DATUM );
+datums.register( ETRS89 );
+datums.register( NAD83 );
