b9c6e90dc8f9f1e650412882e7bc236f24176c1d
komed3 <webmaster@komed3.de>
2026-09-06 11:25:43 +0200

    feat: create src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { Coordinate } from './coordinate/Coordinate';
+import { DMS, DMSDirection } from './coordinate/DMS';
+import { Latitude } from './coordinate/Latitude';
+import { Longitude } from './coordinate/Longitude';
+
+
+export {
+  Coordinate, DMS, DMSDirection, Latitude, Longitude
+};
+
+
+export const geolib = {
+  Coordinate, DMS, Latitude, Longitude
+};
+
+export default geolib;
