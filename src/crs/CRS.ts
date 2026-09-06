764809167e4f02bf66f8d3488d00147b51c2e3d1
komed3 <webmaster@komed3.de>
2026-09-06 23:56:27 +0200

    update CRS.ts

--- src/crs/CRS.ts
+++ src/crs/CRS.ts
@@ -1,30 +1,40 @@
+import { CoordinateSystem } from './CoordinateSystem';
 import { Datum } from './Datum';
+import { Projection } from './Projection';
 
 
 export type CRSType = 'geographic' | 'projected';
-export type CRSUnit = 'degree' | 'metre';
-export type CRSCoordinateSystem = 'ellipsoidal' | 'cartesian';
 
 
 export class CRS {
-  public readonly coordinateSystem: CRSCoordinateSystem;
-
   public constructor (
     public readonly code: string,
     public readonly name: string,
     public readonly type: CRSType,
     public readonly datum: Datum,
-    public readonly unit: CRSUnit
+    public readonly coordinateSystem: CoordinateSystem,
+    public readonly projection?: Projection
   ) {
-    this.coordinateSystem = type === 'geographic' ? 'ellipsoidal' : 'cartesian';
+    if ( type === 'geographic' && projection !== undefined )
+      throw new TypeError( 'Geographic CRS cannot have a projection' );
+
+    if ( type === 'projected' && projection === undefined )
+      throw new TypeError( 'Projected CRS must have a projection' );
   }
 
   public clone () : CRS {
-    return new CRS( this.code, this.name, this.type, this.datum.clone(), this.unit );
+    return new CRS(
+      this.code, this.name, this.type, this.datum.clone(),
+      this.coordinateSystem.clone(), this.projection?.clone()
+    );
   }
 
   public equals ( other: CRS ) : boolean {
-    return this.code === other.code;
+    return this.code === other.code && this.name === other.name && this.type === other.type &&
+      this.datum.equals( other.datum ) && this.coordinateSystem.equals( other.coordinateSystem ) &&
+      ( this.projection === undefined && other.projection === undefined ||
+        this.projection !== undefined && other.projection !== undefined &&
+        this.projection.equals( other.projection ) );
   }
 
   public isGeographic () : boolean {
@@ -35,11 +45,7 @@ export class CRS {
     return this.type === 'projected';
   }
 
-  public isEllipsoidal () : boolean {
-    return this.coordinateSystem === 'ellipsoidal';
-  }
-
-  public isCartesian () : boolean {
-    return this.coordinateSystem === 'cartesian';
+  public hasProjection () : boolean {
+    return this.projection !== undefined;
   }
 }
