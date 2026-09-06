008c7be613a6afe30b2f550249448e15270176a0
komed3 <webmaster@komed3.de>
2026-09-06 19:06:14 +0200

    set ellipsoid

--- src/crs/Ellipsoid.ts
+++ src/crs/Ellipsoid.ts
@@ -1,4 +1,15 @@
 export class Ellipsoid {
   public readonly semiMajorAxis: number;
   public readonly inverseFlattening: number;
+
+  public constructor ( semiMajorAxis: number, inverseFlattening: number ) {
+    if ( ! Number.isFinite( semiMajorAxis ) || semiMajorAxis <= 0 )
+      throw new RangeError( 'Semi-major axis must be a positive finite number' );
+
+    if ( ! Number.isFinite( inverseFlattening ) || inverseFlattening <= 0 )
+      throw new RangeError( 'Inverse flattening must be a positive finite number' );
+
+    this.semiMajorAxis = semiMajorAxis;
+    this.inverseFlattening = inverseFlattening;
+  }
 }
