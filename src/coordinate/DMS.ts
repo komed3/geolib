800e94bcde862f7b7e584aabda42fa90430473f1
komed3 <webmaster@komed3.de>
2026-09-06 11:17:17 +0200

    check range error on degrees

--- src/coordinate/DMS.ts
+++ src/coordinate/DMS.ts
@@ -1,4 +1,9 @@
 export type DMSDirection = 'N' | 'S' | 'E' | 'W';
 
 
-export class DMS {}
+export class DMS {
+  public constructor( degrees: number, minutes: number, seconds: number, dir: DMSDirection ) {
+    if ( ! Number.isInteger( degrees ) || degrees < 0 )
+      throw new RangeError( 'Degrees must be a non-negative integer' );
+  }
+}
