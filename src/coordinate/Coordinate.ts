6028536641b28fc3e58054f2052efa893f848d2e
komed3 <webmaster@komed3.de>
2026-09-06 11:00:41 +0200

    add constructor

--- src/coordinate/Coordinate.ts
+++ src/coordinate/Coordinate.ts
@@ -5,4 +5,9 @@ import { Longitude } from './Longitude';
 export class Coordinate {
   public readonly latitude: Latitude;
   public readonly longitude: Longitude;
+
+  public constructor ( latitude: Latitude, longitude: Longitude ) {
+    this.latitude = latitude;
+    this.longitude = longitude;
+  }
 }
