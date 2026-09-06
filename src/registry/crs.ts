f93719375380502a58803cb704554e94ef73c71d
komed3 <webmaster@komed3.de>
2026-09-07 00:40:14 +0200

    update crs.ts

--- src/registry/crs.ts
+++ src/registry/crs.ts
@@ -1,7 +1,8 @@
 import { CoordinateSystem } from '../crs/CoordinateSystem';
 import { CRS } from '../crs/CRS';
+import { Projection } from '../crs/Projection';
 import { CRSRegistry } from './CRSRegistry';
-import { datums } from './datum';
+import { ETRS89 as ETRS89_D, WGS84 as WGS84_D } from './datum';
 
 
 const geographic2D = new CoordinateSystem( 'ellipsoidal', [
@@ -17,10 +18,10 @@ const projected2D = new CoordinateSystem( 'cartesian', [
 
 export const crs = new CRSRegistry();
 
-export const WGS84 = new CRS(
-  'EPSG:4326',
-  'WGS 84',
-  'geographic',
-  datums.get( 'EPSG:6326' )!,
-  geographic2D
+export const WGS84 = new CRS( 'EPSG:4326', 'WGS 84', 'geographic', WGS84_D, geographic2D );
+export const ETRS89 = new CRS( 'EPSG:4258', 'ETRS89', 'geographic', ETRS89_D, geographic2D );
+
+export const WEB_MERCATOR = new CRS(
+  'EPSG:3857', 'WGS 84 / Pseudo-Mercator', 'projected', WGS84_D, projected2D,
+  new Projection( 'Pseudo-Mercator', 'mercator' )
 );
