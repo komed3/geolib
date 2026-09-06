6559a2f3cd1111e4caf7987319603dbf6295b471
komed3 <webmaster@komed3.de>
2026-09-06 19:10:42 +0200

    feat: create Datum.ts

---
+++ src/crs/Datum.ts
@@ -0,0 +1,17 @@
+import { Ellipsoid } from './Ellipsoid';
+
+
+export class Datum {
+  public constructor (
+    public readonly name: string,
+    public readonly ellipsoid: Ellipsoid
+  ) {}
+
+  public clone () : Datum {
+    return new Datum( this.name, this.ellipsoid.clone() );
+  }
+
+  public equals ( other: Datum ) : boolean {
+    return this.name === other.name && this.ellipsoid.equals( other.ellipsoid );
+  }
+}
