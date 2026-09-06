5e955880856d92ed54358f6f22cdb241d97c4ef3
komed3 <webmaster@komed3.de>
2026-09-06 10:42:34 +0200

    update Latitude.ts

--- src/coordinate/Latitude.ts
+++ src/coordinate/Latitude.ts
@@ -1,1 +1,10 @@
-export class Latitude {}
+export class Latitude {
+  public readonly value: number;
+
+  constructor ( value: number ) {
+    if ( ! Number.isFinite( value ) )
+      throw new TypeError( 'Latitude must be a finite number' );
+
+    this.value = value;
+  }
+}
