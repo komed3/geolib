888b7d5b1949e9c2fb1dd382633ee09e26eac9a7
komed3 <webmaster@komed3.de>
2026-09-06 11:00:02 +0200

    feat: create Coordinate.ts

---
+++ src/coordinate/Coordinate.ts
@@ -0,0 +1,8 @@
+import { Latitude } from './Latitude';
+import { Longitude } from './Longitude';
+
+
+export class Coordinate {
+  public readonly latitude: Latitude;
+  public readonly longitude: Longitude;
+}
