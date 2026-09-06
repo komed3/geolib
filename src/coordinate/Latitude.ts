c6fdf46264b0db54d8f1f9eacbd75cb35e535b73
komed3 <webmaster@komed3.de>
2026-09-06 10:45:27 +0200

    update Latitude.ts

--- src/coordinate/Latitude.ts
+++ src/coordinate/Latitude.ts
@@ -10,4 +10,12 @@ export class Latitude {
 
     this.value = value;
   }
+
+  public toRadians () : number {
+    return this.value * Math.PI / 180;
+  }
+
+  public toString ( precision: number = -1 ) : string {
+    return precision < 0 ? `${ this.value }` : this.value.toFixed( precision );
+  }
 }
