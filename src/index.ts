7198f55d264daacd191883d96bbd449ecc8e3e72
komed3 <webmaster@komed3.de>
2026-09-07 00:49:57 +0200

    re-export registries

--- src/index.ts
+++ src/index.ts
@@ -10,15 +10,28 @@ import { Datum } from './crs/Datum';
 import { Ellipsoid } from './crs/Ellipsoid';
 import { Projection } from './crs/Projection';
 
+import {
+  CLARKE1866, crs, CRSRegistry, DatumRegistry, datums, EllipsoidRegistry, ellipsoids, ETRS89,
+  ETRS89_D, GRS80, NAD83, Registry, WEB_MERCATOR, WGS84, WGS84_D, WGS84_E, WGS84_UTM32N
+} from './registry';
+
 
 export {
-  AxisDirection, AxisUnit, Coordinate, CoordinateAxis, CoordinateSystem, CoordinateSystemType,
-  CRS, CRSType, Datum, DMS, DMSDirection, Ellipsoid, Latitude, Longitude, Projection
+  AxisDirection, AxisUnit, CLARKE1866, Coordinate, CoordinateAxis, CoordinateSystem,
+  CoordinateSystemType, CRS, crs, CRSRegistry, CRSType, Datum, DatumRegistry, datums,
+  DMS, DMSDirection, Ellipsoid, EllipsoidRegistry, ellipsoids, ETRS89, ETRS89_D, GRS80,
+  Latitude, Longitude, NAD83, Projection, Registry, WEB_MERCATOR, WGS84, WGS84_D,
+  WGS84_E, WGS84_UTM32N
 };
 
 
 export const geolib = {
-  Coordinate, CoordinateSystem, CRS, Datum, DMS, Ellipsoid, Latitude, Longitude, Projection
+  Coordinate, CoordinateSystem, CRS, Datum, DMS, Ellipsoid, Latitude, Longitude, Projection,
+  CRSRegistry, DatumRegistry, EllipsoidRegistry, Registry,
+  registry: { crs, datums, ellipsoids },
+  crs: { WGS84, ETRS89, WEB_MERCATOR, WGS84_UTM32N },
+  ellipsoids: { WGS84: WGS84_E, GRS80, CLARKE1866 },
+  datums: { WGS84: WGS84_D, ETRS89: ETRS89_D, NAD83 }
 };
 
 export default geolib;
