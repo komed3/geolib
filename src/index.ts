ffbbe9b310b246c8baa1c6cb39798d5a160df525
komed3 <webmaster@komed3.de>
2026-09-07 00:23:23 +0200

    export CRS types and classes

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
 import { Coordinate } from './coordinate/Coordinate';
-import { DMS, DMSDirection } from './coordinate/DMS';
+import { DMS, type DMSDirection } from './coordinate/DMS';
 import { Latitude } from './coordinate/Latitude';
 import { Longitude } from './coordinate/Longitude';
 
+import type { AxisDirection, AxisUnit, CoordinateAxis, CoordinateSystemType } from './crs/CoordinateSystem';
+import { CoordinateSystem } from './crs/CoordinateSystem';
+import { CRS, type CRSType } from './crs/CRS';
+import { Datum } from './crs/Datum';
+import { Ellipsoid } from './crs/Ellipsoid';
+import { Projection } from './crs/Projection';
+
 
 export {
-  Coordinate, DMS, DMSDirection, Latitude, Longitude
+  AxisDirection, AxisUnit, Coordinate, CoordinateAxis, CoordinateSystem, CoordinateSystemType,
+  CRS, CRSType, Datum, DMS, DMSDirection, Ellipsoid, Latitude, Longitude, Projection
 };
 
 
 export const geolib = {
-  Coordinate, DMS, Latitude, Longitude
+  Coordinate, CoordinateSystem, CRS, Datum, DMS, Ellipsoid, Latitude, Longitude, Projection
 };
 
 export default geolib;
