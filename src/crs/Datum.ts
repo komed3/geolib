71443e04c4bd8e9e0101409ae2c8be310d5cacb2
komed3 <webmaster@komed3.de>
2026-09-06 23:14:05 +0200

    update Datum.ts

--- src/crs/Datum.ts
+++ src/crs/Datum.ts
@@ -3,15 +3,17 @@ import { Ellipsoid } from './Ellipsoid';
 
 export class Datum {
   public constructor (
+    public readonly code: string,
     public readonly name: string,
     public readonly ellipsoid: Ellipsoid
   ) {}
 
   public clone () : Datum {
-    return new Datum( this.name, this.ellipsoid.clone() );
+    return new Datum( this.code, this.name, this.ellipsoid.clone() );
   }
 
   public equals ( other: Datum ) : boolean {
-    return this.name === other.name && this.ellipsoid.equals( other.ellipsoid );
+    return this.code === other.code && this.name === other.name &&
+      this.ellipsoid.equals( other.ellipsoid );
   }
 }
