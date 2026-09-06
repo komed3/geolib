04803297f1ba6b150a77f95c372114f4cd312eae
komed3 <webmaster@komed3.de>
2026-09-07 00:10:45 +0200

    implement getter

--- src/crs/Registry.ts
+++ src/crs/Registry.ts
@@ -6,6 +6,14 @@ interface RegistryEntry {
 export abstract class Registry< T extends RegistryEntry > {
   protected readonly entries = new Map< string, T >();
 
+  public values () : T[] {
+    return [ ...this.entries.values() ];
+  }
+
+  public get size () : number {
+    return this.entries.size;
+  }
+
   public register ( entry: T ) : void {
     this.entries.set( entry.code, entry );
   }
