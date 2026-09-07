e1c7e0d7e288f7a72037c4d4f3f6a4fb71abba6a
komed3 <webmaster@komed3.de>
2026-09-07 09:10:12 +0200

    declare cls

--- src/projection/ProjectedCoordinate.ts
+++ src/projection/ProjectedCoordinate.ts
@@ -2,3 +2,10 @@ export type ProjectedTuple = [
   easting: number,
   northing: number
 ];
+
+export class ProjectedCoordinate {
+  public constructor (
+    public readonly easting: number,
+    public readonly northing: number
+  ) {}
+}
