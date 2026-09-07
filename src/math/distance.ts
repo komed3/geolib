68aedde9c0093f22ade53d09c4dc7e2e34819f9f
komed3 <webmaster@komed3.de>
2026-09-08 00:20:14 +0200

    calculate distance

--- src/math/distance.ts
+++ src/math/distance.ts
@@ -9,3 +9,18 @@ export interface DistanceOptions {
 }
 
 
+export function distance ( a: Coordinate, b: Coordinate, options: DistanceOptions ) : number;
+export function distance ( a: ProjectedCoordinate, b: ProjectedCoordinate ) : number;
+
+export function distance (
+  a: Coordinate | ProjectedCoordinate, b: Coordinate | ProjectedCoordinate,
+  { ellipsoid }: DistanceOptions = {}
+) : number {
+  if ( a instanceof ProjectedCoordinate && b instanceof ProjectedCoordinate )
+    return Math.hypot( b.easting - a.easting, b.northing - a.northing );
+
+  if ( a instanceof ProjectedCoordinate || b instanceof ProjectedCoordinate )
+    throw new TypeError( 'Coordinates must use the same coordinate type' );
+
+  return geodesic( a, b, ellipsoid ).distance;
+}
