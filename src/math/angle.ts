867bf1641c2532f1780c79f2472c78801e741d02
komed3 <webmaster@komed3.de>
2026-09-08 00:22:19 +0200

    overload angle

--- src/math/angle.ts
+++ src/math/angle.ts
@@ -8,3 +8,7 @@ import { geodesic } from './geodesic';
 export interface AngleOptions {
   readonly ellipsoid?: Ellipsoid;
 }
+
+
+export function angle ( a: Coordinate, b: Coordinate, c: Coordinate, { ellipsoid }: AngleOptions ) : number;
+export function angle ( a: ProjectedCoordinate, b: ProjectedCoordinate, c: ProjectedCoordinate ) : number;
