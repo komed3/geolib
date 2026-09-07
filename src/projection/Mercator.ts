883abb7dbf98b2bb4abe8165e40e7b868cee73f2
komed3 <webmaster@komed3.de>
2026-09-07 09:20:00 +0200

    unproject mercator

--- src/projection/Mercator.ts
+++ src/projection/Mercator.ts
@@ -1,4 +1,4 @@
-import type { Coordinate } from '../coordinate/Coordinate';
+import { Coordinate } from '../coordinate/Coordinate';
 import type { Ellipsoid } from '../crs/Ellipsoid';
 import { deg2Rad } from '../utils/math';
 import { ProjectedCoordinate } from './ProjectedCoordinate';
@@ -45,5 +45,27 @@ export class Mercator extends Projection {
     ) + this.falseNorthing;
 
     return new ProjectedCoordinate( x, y );
+  }
+
+  public unproject ( { easting, northing }: ProjectedCoordinate ) : Coordinate {
+    const eccentricity = this.ellipsoid.firstEccentricity;
+    const x = ( easting - this.falseEasting ) / ( this.ellipsoid.semiMajorAxis * this.scaleFactor );
+    const y = ( northing - this.falseNorthing ) / ( this.ellipsoid.semiMajorAxis * this.scaleFactor );
+
+    const longitude = deg2Rad( this.centralMeridian ) + x;
+    let latitude = Math.PI / 2 - 2 * Math.atan( Math.exp( -y ) );
+
+    for ( let i = 0; i < 10; i++ ) {
+      const sinLatitude = Math.sin( latitude );
+
+      const next = Math.PI / 2 - 2 * Math.atan( Math.exp( -y ) * (
+        ( 1 - eccentricity * sinLatitude ) / ( 1 + eccentricity * sinLatitude )
+      ) ** ( eccentricity / 2 ) );
+
+      if ( Math.abs( next - latitude ) < 1e-12 ) { latitude = next; break }
+      latitude = next;
+    }
+
+    return Coordinate.fromRadians( latitude, longitude );
   }
 }
