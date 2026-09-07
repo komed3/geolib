73066e467084a6479fe6477f02db78ac18fd9cbd
komed3 <webmaster@komed3.de>
2026-09-07 09:14:52 +0200

    feat: create Mercator.ts

---
+++ src/projection/Mercator.ts
@@ -0,0 +1,15 @@
+import type { Ellipsoid } from '../crs/Ellipsoid';
+import { Projection } from './Projection';
+
+
+export class Mercator extends Projection {
+  public constructor (
+    public readonly ellipsoid: Ellipsoid,
+    public readonly centralMeridian: number = 0,
+    public readonly scaleFactor: number = 1,
+    public readonly falseEasting: number = 0,
+    public readonly falseNorthing: number = 0
+  ) {
+    super( 'Mercator' );
+  }
+}
