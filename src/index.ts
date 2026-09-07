ba0ee13354130d49fcaf9c351c8f4914e4d72f5e
komed3 <webmaster@komed3.de>
2026-09-08 00:38:38 +0200

    manage lib exports

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,9 @@ import { CRS, type CRSType } from './crs/CRS';
 import { Datum } from './crs/Datum';
 import { Ellipsoid } from './crs/Ellipsoid';
 
-import { angle } from './math/angle';
-import { bearing } from './math/bearing';
-import { distance } from './math/distance';
+import { angle, type AngleOptions } from './math/angle';
+import { bearing, type BearingOptions } from './math/bearing';
+import { distance, type DistanceOptions } from './math/distance';
 
 import { Mercator } from './projection/Mercator';
 import { ProjectedCoordinate, type ProjectedTuple } from './projection/ProjectedCoordinate';
@@ -25,11 +25,11 @@ import {
 
 
 export {
-  angle, AxisDirection, AxisUnit, bearing, CLARKE1866, Coordinate, CoordinateAxis,
-  CoordinateSystem, CoordinateSystemType, CRS, crs, CRSRegistry, CRSType, Datum,
-  DatumRegistry, datums, distance, DMS, DMSDirection, Ellipsoid, EllipsoidRegistry,
-  ellipsoids, ETRS89, ETRS89_D, GRS80, Latitude, Longitude, Mercator, NAD83,
-  ProjectedCoordinate, ProjectedTuple, SphericalMercator, TransverseMercator,
+  angle, AngleOptions, AxisDirection, AxisUnit, bearing, BearingOptions, CLARKE1866,
+  Coordinate, CoordinateAxis, CoordinateSystem, CoordinateSystemType, CRS, crs, CRSRegistry,
+  CRSType, Datum, DatumRegistry, datums, distance, DistanceOptions, DMS, DMSDirection,
+  Ellipsoid, EllipsoidRegistry, ellipsoids, ETRS89, ETRS89_D, GRS80, Latitude, Longitude,
+  Mercator, NAD83, ProjectedCoordinate, ProjectedTuple, SphericalMercator, TransverseMercator,
   Tuple, WEB_MERCATOR, WGS84, WGS84_D, WGS84_E, WGS84_UTM32N
 };
 
