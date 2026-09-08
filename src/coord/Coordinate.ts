dba962b5372cf8e820d89314be959c466eb1bd66
komed3 <webmaster@komed3.de>
2026-09-08 16:13:48 +0200

    implement Coordinate cls

--- src/coord/Coordinate.ts
+++ src/coord/Coordinate.ts
@@ -6,3 +6,14 @@ export interface ICoordinate {
   readonly values: readonly number[];
   readonly system: System;
 }
+
+
+export class Coordinate implements ICoordinate {
+  public readonly values: readonly number[];
+  public readonly system: System;
+
+  public constructor ( { values, system }: ICoordinate ) {
+    this.values = Object.freeze( [ ...values ] );
+    this.system = system;
+  }
+}
