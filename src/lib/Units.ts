499336a544315742334a527142e20cc67c1a91a9
komed3 <webmaster@komed3.de>
2026-09-08 15:51:17 +0200

    feat: create Units.ts

---
+++ src/lib/Units.ts
@@ -0,0 +1,9 @@
+export interface Unit {
+  name: string;
+  symbol: string;
+}
+
+
+export const DEGREE: Unit = { name: 'Degree', symbol: '°' };
+export const RADIAN: Unit = { name: 'Radian', symbol: 'rad' };
+export const METRE: Unit = { name: 'Metre', symbol: 'm' };
