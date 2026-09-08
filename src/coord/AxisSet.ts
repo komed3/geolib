68765b6181a392c8a3a6d1817be00812d0502a9e
komed3 <webmaster@komed3.de>
2026-09-09 00:13:55 +0200

    setup `AxisSet` class

--- src/coord/AxisSet.ts
+++ src/coord/AxisSet.ts
@@ -5,3 +5,12 @@ export interface TAxisSetStringOptions {
   showUnit?: boolean;
   delimiter?: string;
 }
+
+
+export class AxisSet {
+  public readonly axes: readonly Axis[];
+
+  public constructor ( axes: readonly Axis[] ) {
+    this.axes = Object.freeze( [ ...axes ] );
+  }
+}
