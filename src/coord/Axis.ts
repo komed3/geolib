0715a550b9a69fe1120373fd87227be2f0586501
komed3 <webmaster@komed3.de>
2026-09-08 15:59:46 +0200

    feat: create Axis.ts

---
+++ src/coord/Axis.ts
@@ -0,0 +1,6 @@
+import type { IUnit } from '../lib/Units';
+import { Utils } from '../lib/Utils';
+import type { IRange, Range } from './Range';
+
+
+export type TAxisNormalization = 'none' | 'clamp' | 'wrap';
