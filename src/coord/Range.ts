133c4e5e84247a8d42529e769a13f937712c19c0
komed3 <webmaster@komed3.de>
2026-09-08 15:55:20 +0200

    update Range.ts

--- src/coord/Range.ts
+++ src/coord/Range.ts
@@ -1,6 +1,23 @@
-export interface Range {
+export interface RangeProps {
   readonly min?: number;
   readonly max?: number;
   readonly minInclusive?: boolean;
   readonly maxInclusive?: boolean;
+}
+
+
+export class Range {
+  public readonly min: number;
+  public readonly max: number;
+  public readonly minInclusive: boolean;
+  public readonly maxInclusive: boolean;
+
+  public constructor ( { min = -Infinity, max = Infinity, minInclusive = true, maxInclusive = true }: RangeProps = {} ) {
+    if ( min > max ) throw new Error( `Range: min (${min}) cannot be greater than max (${max})` );
+
+    this.min = min;
+    this.max = max;
+    this.minInclusive = minInclusive;
+    this.maxInclusive = maxInclusive;
+  }
 }
