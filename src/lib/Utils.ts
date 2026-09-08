93f2577ec24153ca5a66e61dfc6d0bd93aeb559d
komed3 <webmaster@komed3.de>
2026-09-08 15:47:53 +0200

    update Utils.ts

--- src/lib/Utils.ts
+++ src/lib/Utils.ts
@@ -14,4 +14,8 @@ export class Utils {
   public static normalize ( value: number, min: number, max: number, mode: NormalizeMode = 'none' ) : number {
     return mode === 'clamp' ? this.clamp( value, min, max ) : mode === 'wrap' ? this.wrap( value, min, max ) : value;
   }
+
+  public static approximatelyEqual ( a: number, b: number, tolerance = Number.EPSILON ) : boolean {
+    return Math.abs( a - b ) <= tolerance;
+  }
 }
