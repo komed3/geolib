1295bbd2abfbeeaf0aa5ad0ed8ec311e57ba7d1f
komed3 <webmaster@komed3.de>
2026-09-08 20:15:31 +0200

    update Units.ts

--- src/lib/Units.ts
+++ src/lib/Units.ts
@@ -1,9 +1,14 @@
 export interface TUnit {
-  name: string;
-  symbol: string;
+  readonly name: string;
+  readonly symbol: string;
 }
 
 
 export const DEGREE: TUnit = { name: 'Degree', symbol: '°' };
 export const RADIAN: TUnit = { name: 'Radian', symbol: 'rad' };
 export const METRE: TUnit = { name: 'Metre', symbol: 'm' };
+export const KILOMETRE: TUnit = { name: 'Kilometre', symbol: 'km' };
+export const CENTIMETRE: TUnit = { name: 'Centimetre', symbol: 'cm' };
+export const MILLIMETRE: TUnit = { name: 'Millimetre', symbol: 'mm' };
+export const FOOT: TUnit = { name: 'Foot', symbol: 'ft' };
+export const US_SURVEY_FOOT: TUnit = { name: 'US Survey Foot', symbol: 'ftUS' };
