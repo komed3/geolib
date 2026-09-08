08eb23b766f041306e711f87726600ff3a42874a
komed3 <webmaster@komed3.de>
2026-09-09 00:20:30 +0200

    implement `System` cls

--- src/coord/System.ts
+++ src/coord/System.ts
@@ -5,3 +5,23 @@ export interface TSystemStringOptions {
   showUnit?: boolean;
   delimiter?: string;
 }
+
+
+export class System {
+  public constructor (
+    public readonly name: string,
+    public readonly axes: AxisSet
+  ) {}
+
+  public get dimension () : number {
+    return this.axes.dimension;
+  }
+
+  public equals ( system: System ) : boolean {
+    return this.name === system.name && this.axes.equals( system.axes );
+  }
+
+  public toString ( options: TSystemStringOptions = {} ) : string {
+    return `${ this.name } (${ this.axes.toString( options ) })`;
+  }
+}
