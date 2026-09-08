55c3c9b332ace2a1f78fcbc996d3898a16ea6abf
komed3 <webmaster@komed3.de>
2026-09-09 00:23:17 +0200

    feat: create Coordinate.ts

---
+++ src/coord/Coordinate.ts
@@ -0,0 +1,7 @@
+import type { System } from './System';
+import type { Value, TValueStringOptions } from './Value';
+
+
+export interface TCoordinateStringOptions extends TValueStringOptions {
+  delimiter?: string;
+}
