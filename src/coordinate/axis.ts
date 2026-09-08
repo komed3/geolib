7e935ae1ce62f66b99a65fe4aa67ffb9317e4d90
komed3 <webmaster@komed3.de>
2026-09-08 15:31:56 +0200

    feat: create axis.ts

---
+++ src/coordinate/axis.ts
@@ -0,0 +1,6 @@
+import { normalize, type NormalizationMode } from '../math/normalize';
+import type { Unit } from '../units/unit';
+import { Range, type RangeConfig } from './range';
+
+
+export type AxisDirection = string;
