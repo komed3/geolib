7c48ad09e0d6310ada51449c30623842bd95616a
komed3 <webmaster@komed3.de>
2026-09-08 15:47:35 +0200

    normalize values

--- src/lib/Utils.ts
+++ src/lib/Utils.ts
@@ -1,3 +1,6 @@
+export type NormalizeMode = 'none' | 'clamp' | 'wrap';
+
+
 export class Utils {
   public static clamp ( value: number, min: number, max: number ) : number {
     return Math.min( Math.max( value, min ), max );
@@ -6,5 +9,9 @@ export class Utils {
   public static wrap ( value: number, min: number, max: number ) : number {
     const range = max - min;
     return ( ( value - min ) % range + range ) % range + min;
+  }
+
+  public static normalize ( value: number, min: number, max: number, mode: NormalizeMode = 'none' ) : number {
+    return mode === 'clamp' ? this.clamp( value, min, max ) : mode === 'wrap' ? this.wrap( value, min, max ) : value;
   }
 }
