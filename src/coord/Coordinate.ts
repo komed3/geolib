2f4ef94d44c959673b996f984bc1ee6150deeb55
komed3 <webmaster@komed3.de>
2026-09-09 00:23:56 +0200

    init coordinate cls

--- src/coord/Coordinate.ts
+++ src/coord/Coordinate.ts
@@ -5,3 +5,13 @@ import type { Value, TValueStringOptions } from './Value';
 export interface TCoordinateStringOptions extends TValueStringOptions {
   delimiter?: string;
 }
+
+
+export class Coordinate {
+  public readonly system: System;
+  public readonly values: readonly Value[];
+
+  public constructor ( system: System, values: readonly Value[] ) {
+    this.system = system, this.values = Object.freeze( [ ...values ] );
+  }
+}
