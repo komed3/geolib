a0b3a6831dc6e545ed389203c9d28a747953ccda
komed3 <webmaster@komed3.de>
2026-09-09 00:14:22 +0200

    update AxisSet.ts

--- src/coord/AxisSet.ts
+++ src/coord/AxisSet.ts
@@ -13,4 +13,20 @@ export class AxisSet {
   public constructor ( axes: readonly Axis[] ) {
     this.axes = Object.freeze( [ ...axes ] );
   }
+
+  public get dimension () : number {
+    return this.axes.length;
+  }
+
+  public get ( index: number ) : Axis {
+    return this.axes[ index ];
+  }
+
+  public indexOf ( axis: Axis ) : number {
+    return this.axes.findIndex( item => item.equals( axis ) );
+  }
+
+  public has ( axis: Axis ) : boolean {
+    return this.indexOf( axis ) !== -1;
+  }
 }
