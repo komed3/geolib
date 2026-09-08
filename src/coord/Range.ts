4c6ede1b55a4ccd30554629dc89b4a878205cc43
komed3 <webmaster@komed3.de>
2026-09-08 23:48:52 +0200

    update Range.ts

--- src/coord/Range.ts
+++ src/coord/Range.ts
@@ -4,3 +4,15 @@ export interface TRangeOptions {
   minInclusive?: boolean;
   maxInclusive?: boolean;
 }
+
+
+export class Range {
+  public readonly min: number | null;
+  public readonly max: number | null;
+  public readonly minInclusive: boolean;
+  public readonly maxInclusive: boolean;
+
+  public constructor ( { min = null, max = null, minInclusive = true, maxInclusive = true }: TRangeOptions = {} ) {
+    this.min = min, this.max = max, this.minInclusive = minInclusive, this.maxInclusive = maxInclusive;
+  }
+}
