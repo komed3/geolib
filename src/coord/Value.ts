2f7e2ac204ad72d00d7a82f1ea1bf606d1151b2f
komed3 <webmaster@komed3.de>
2026-09-09 00:10:00 +0200

    update Value.ts

--- src/coord/Value.ts
+++ src/coord/Value.ts
@@ -15,4 +15,12 @@ export class Value {
   public constructor ( value: number, axis: Axis ) {
     this.axis = axis, this.value = axis.initialize( value );
   }
+
+  public toNumber () : number {
+    return this.value;
+  }
+
+  public equals ( value: Value ) : boolean {
+    return this.value === value.value && this.axis.equals( value.axis );
+  }
 }
