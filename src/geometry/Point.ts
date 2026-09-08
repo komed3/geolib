d77b93af92773f7bd21a5e903f0650a7cc3471c6
komed3 <webmaster@komed3.de>
2026-09-08 10:57:05 +0200

    clone / equals

--- src/geometry/Point.ts
+++ src/geometry/Point.ts
@@ -5,3 +5,16 @@ import type { ProjectedCoordinate } from '../projection/ProjectedCoordinate';
 
 
 export type GeometryCoordinate = Coordinate | ProjectedCoordinate;
+
+
+export class Point< T extends GeometryCoordinate = GeometryCoordinate > {
+  public constructor ( public readonly coordinate: T ) {}
+
+  public clone () : Point< T > {
+    return new Point( this.coordinate.clone() as T );
+  }
+
+  public equals ( other: Point< T > ) : boolean {
+    return this.coordinate.equals( other.coordinate );
+  }
+}
