63aaa97c5f57f5e0eae464e2e6dac85e5b99fec5
komed3 <webmaster@komed3.de>
2026-09-08 15:46:36 +0200

    implement wrapper

--- src/lib/Utils.ts
+++ src/lib/Utils.ts
@@ -2,4 +2,9 @@ export class Utils {
   public static clamp ( value: number, min: number, max: number ) : number {
     return Math.min( Math.max( value, min ), max );
   }
+
+  public static wrap ( value: number, min: number, max: number ) : number {
+    const range = max - min;
+    return ( ( value - min ) % range + range ) % range + min;
+  }
 }
