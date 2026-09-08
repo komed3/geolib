30679d372ee8f669448d0fd9886f4db507f0760c
komed3 <webmaster@komed3.de>
2026-09-08 15:57:20 +0200

    update Range.ts

--- src/coord/Range.ts
+++ src/coord/Range.ts
@@ -12,12 +12,34 @@ export class Range {
   public readonly minInclusive: boolean;
   public readonly maxInclusive: boolean;
 
-  public constructor ( { min = -Infinity, max = Infinity, minInclusive = true, maxInclusive = true }: RangeProps = {} ) {
+  public constructor ( { min = -Infinity, max = Infinity, minInclusive, maxInclusive }: RangeProps = {} ) {
     if ( min > max ) throw new Error( `Range: min (${min}) cannot be greater than max (${max})` );
 
-    this.min = min;
-    this.max = max;
-    this.minInclusive = minInclusive;
-    this.maxInclusive = maxInclusive;
+    this.min = min, this.max = max;
+    this.minInclusive = minInclusive ?? true;
+    this.maxInclusive = maxInclusive ?? true;
+  }
+
+  public contains ( value: number ) : boolean {
+    return this.minInclusive ? value >= this.min : value > this.min &&
+      this.maxInclusive ? value <= this.max : value < this.max;
+  }
+
+  public clamp ( value: number ) : number {
+    return value < this.min ? this.min : value > this.max ? this.max : value;
+  }
+
+  public get bounded () : boolean {
+    return this.min !== -Infinity || this.max !== Infinity;
+  }
+
+  public get size () : number {
+    return this.max - this.min;
+  }
+
+  public equals ( range: Range ) : boolean {
+    return this.min === range.min && this.max === range.max &&
+      this.minInclusive === range.minInclusive &&
+      this.maxInclusive === range.maxInclusive;
   }
 }
