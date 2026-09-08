fd39d47135d1dc097e2c8d73092bbf5e8726b0d9
komed3 <webmaster@komed3.de>
2026-09-08 16:07:29 +0200

    implement axis set methods

--- src/coord/AxisSet.ts
+++ src/coord/AxisSet.ts
@@ -7,4 +7,20 @@ export class AxisSet {
   public constructor ( axes: readonly Axis[] ) {
     this.axes = Object.freeze( [ ...axes ] );
   }
+
+  public get dimension () : number {
+    return this.axes.length;
+  }
+
+  public get ( index: number ) : Axis | undefined {
+    return this.axes[ index ];
+  }
+
+  public indexOf ( axis: Axis ) : number {
+    return this.axes.indexOf( axis );
+  }
+
+  public has ( axis: Axis ) : boolean {
+    return this.indexOf( axis ) !== -1;
+  }
 }
