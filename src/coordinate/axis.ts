5f68ba0812d6f67752856c7e3984e7da1a1d49c2
komed3 <webmaster@komed3.de>
2026-09-08 15:32:21 +0200

    update axis.ts and range.ts

--- src/coordinate/axis.ts
+++ src/coordinate/axis.ts
@@ -4,3 +4,13 @@ import { Range, type RangeConfig } from './range';
 
 
 export type AxisDirection = string;
+
+export interface AxisConfig {
+  readonly name: string;
+  readonly direction: AxisDirection;
+  readonly unit?: Unit;
+  readonly range?: Range | RangeConfig;
+  readonly normalization?: NormalizationMode;
+  readonly cyclic?: boolean;
+  readonly period?: number;
+}
--- src/coordinate/range.ts
+++ src/coordinate/range.ts
@@ -2,10 +2,10 @@ import { clamp } from '../math/clamp';
 
 
 export interface RangeConfig {
-  readonly min?: number
-  readonly max?: number
-  readonly minInclusive?: boolean
-  readonly maxInclusive?: boolean
+  readonly min?: number;
+  readonly max?: number;
+  readonly minInclusive?: boolean;
+  readonly maxInclusive?: boolean;
 }
 
 
