98cbefd6ca872e005ab3355bfbe7a702847ba5e6
komed3 <webmaster@komed3.de>
2026-09-08 11:02:37 +0200

    get length

--- src/geometry/LineString.ts
+++ src/geometry/LineString.ts
@@ -8,4 +8,13 @@ export class LineString< T extends GeometryCoordinate > {
     if ( points.length < 2 ) throw new RangeError( 'LineString must contain at least two points' );
     this.points = points.map( point => point.clone() );
   }
+
+  public get length () : number {
+    let total = 0;
+
+    for ( let i = 1; i < this.points.length; i++ )
+      total += this.points[ i - 1 ].distanceTo( this.points[ i ] );
+
+    return total;
+  }
 }
