fffcc6800363c01afd33ffede03081006636c428
komed3 <webmaster@komed3.de>
2026-09-08 16:11:13 +0200

    update System.ts

--- src/coord/System.ts
+++ src/coord/System.ts
@@ -1,9 +1,21 @@
 import type { Axis } from './Axis';
-import type { AxisSet } from './AxisSet';
+import { AxisSet } from './AxisSet';
 
 
-export interface ICoordinateSystem {
+export interface ISystem {
   readonly name?: string;
   readonly type?: string;
   readonly axes: AxisSet | readonly Axis[];
+}
+
+
+export class System {
+  public readonly name?: string;
+  public readonly type?: string;
+  public readonly axes: AxisSet;
+
+  public constructor ( { name, type, axes }: ISystem ) {
+    this.name = name, this.type = type;
+    this.axes = axes instanceof AxisSet ? axes : new AxisSet( axes );
+  }
 }
