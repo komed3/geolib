aeac1dc7753a66962510aa2003e08a3407d5d324
komed3 <webmaster@komed3.de>
2026-09-08 15:28:43 +0200

    declare range class

--- src/coordinate/range.ts
+++ src/coordinate/range.ts
@@ -7,3 +7,22 @@ export interface RangeConfig {
   readonly minInclusive?: boolean
   readonly maxInclusive?: boolean
 }
+
+
+export class Range {
+  readonly min: number
+  readonly max: number
+  readonly minInclusive: boolean
+  readonly maxInclusive: boolean
+
+  constructor ( { min = -Infinity, max = Infinity, minInclusive = true, maxInclusive = true }: RangeConfig = {} ) {
+    if ( min > max ) throw new RangeError( 'Range minimum must not exceed maximum' );
+
+    this.min = min;
+    this.max = max;
+    this.minInclusive = minInclusive;
+    this.maxInclusive = maxInclusive;
+
+    Object.freeze( this );
+  }
+}
