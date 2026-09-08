f89c088a07c78b1d4d990c789d1c3a9066ce3a50
komed3 <webmaster@komed3.de>
2026-09-08 20:13:12 +0200

    update Utils.ts

--- src/lib/Utils.ts
+++ src/lib/Utils.ts
@@ -1,9 +1,10 @@
-export type TNormalizeMode = 'none' | 'clamp' | 'wrap';
-
-
 export class Utils {
   public static clamp ( value: number, min: number, max: number ) : number {
     return Math.min( Math.max( value, min ), max );
+  }
+
+  public static normalize ( value: number, min: number, max: number ) : number {
+    return this.wrap( value, min, max );
   }
 
   public static wrap ( value: number, min: number, max: number ) : number {
@@ -11,19 +12,23 @@ export class Utils {
     return ( ( value - min ) % range + range ) % range + min;
   }
 
-  public static normalize ( value: number, min: number, max: number, mode: TNormalizeMode = 'none' ) : number {
-    return mode === 'clamp' ? this.clamp( value, min, max ) : mode === 'wrap' ? this.wrap( value, min, max ) : value;
-  }
-
   public static approximatelyEqual ( a: number, b: number, tolerance = Number.EPSILON ) : boolean {
     return Math.abs( a - b ) <= tolerance;
   }
 
-  public static min ( values: readonly number[] ) : number {
+  public static min ( ...values: number[] ) : number {
     return Math.min( ...values );
   }
 
-  public static max ( values: readonly number[] ) : number {
+  public static max ( ...values: number[] ) : number {
     return Math.max( ...values );
+  }
+
+  public static sum ( ...values: number[] ) : number {
+    return values.reduce( ( sum, value ) => sum + value, 0 );
+  }
+
+  public static average ( ...values: number[] ) : number {
+    return this.sum( ...values ) / values.length;
   }
 }
