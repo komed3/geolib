95197346f9daf456a14af8bc1d92fba3823dbce3
komed3 <webmaster@komed3.de>
2026-09-09 11:19:53 +0200

    feat: create Longitude360Axis.ts

---
+++ src/coord/axis/Longitude360Axis.ts
@@ -0,0 +1,13 @@
+import { Axis } from '../../base/Axis';
+import { DEGREE } from '../../lib/Units';
+import { Longitude360Range } from '../range/Longitude360Range';
+
+
+export class Longitude360Axis extends Axis {
+  public constructor () {
+    super( {
+      name: 'Longitude', direction: 'east', unit: DEGREE,
+      range: new Longitude360Range(), behavior: 'wrap'
+    } );
+  }
+}
