87d961c11f8b58fb53bece7692ef0aa82ceddbed
komed3 <webmaster@komed3.de>
2026-09-09 10:10:02 +0200

    feat: create EastingAxis.ts

---
+++ src/coord/EastingAxis.ts
@@ -0,0 +1,16 @@
+import { Axis } from '../base/Axis';
+import { Range } from '../base/Range';
+import { METRE } from '../lib/Units';
+
+
+export class EastingAxis extends Axis {
+  public constructor () {
+    super( {
+      name: 'Easting',
+      direction: 'east',
+      unit: METRE,
+      range: new Range(),
+      behavior: 'none'
+    } );
+  }
+}
