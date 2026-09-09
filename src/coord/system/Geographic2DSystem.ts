99ebbc1c1ac85e2a91db4f2312af3f81a421ca3a
komed3 <webmaster@komed3.de>
2026-09-09 10:24:00 +0200

    feat: create Geographic2DSystem.ts

---
+++ src/coord/system/Geographic2DSystem.ts
@@ -0,0 +1,13 @@
+import { AxisSet } from '../../base/AxisSet';
+import { System } from '../../base/System';
+import { LatitudeAxis } from '../axis/LatitudeAxis';
+import { LongitudeAxis } from '../axis/LongitudeAxis';
+
+
+export class Geographic2DSystem extends System {
+  public constructor ( name = 'Geographic 2D', axes = new AxisSet( [
+    new LongitudeAxis(), new LatitudeAxis()
+  ] ) ) {
+    super( name, axes );
+  }
+}
