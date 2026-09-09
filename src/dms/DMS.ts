26fc9731789750f340a344298c3915a0b487fe13
komed3 <webmaster@komed3.de>
2026-09-09 14:06:29 +0200

    DMS to decimal

--- src/dms/DMS.ts
+++ src/dms/DMS.ts
@@ -8,4 +8,10 @@ export class DMS {
     public readonly seconds: number,
     public readonly direction: TDirection | null = null
   ) {}
+
+  public toDecimal () : number {
+    return ( this.degrees < 0 ? -1 : 1 ) * (
+      Math.abs( this.degrees ) + this.minutes / 60 + this.seconds / 3600
+    );
+  }
 }
