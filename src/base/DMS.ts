c3a0acdb31e649384b9925742392451e444c7934
komed3 <webmaster@komed3.de>
2026-09-09 23:17:19 +0200

    feat: create DMS.ts

---
+++ src/base/DMS.ts
@@ -0,0 +1,24 @@
+export type TDirection = 'north' | 'east' | 'south' | 'west';
+
+export type TDMSFormat = 'dd' | 'dm' | 'dms';
+
+export type TNotation = 'signed' | 'directional';
+
+export type TDirectionMap = Partial< Record< TDirection, string > >;
+
+export interface TDMSOptions {
+  degrees: number;
+  minutes?: number;
+  seconds?: number;
+  direction?: TDirection | null;
+}
+
+export interface TDMSStringOptions {
+  format?: TDMSFormat;
+  locale?: string;
+  precision?: number;
+  delimiter?: string;
+  showUnit?: boolean;
+  dirMap?: TDirectionMap;
+  notation?: TNotation;
+}
