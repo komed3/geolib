fad4e33ca18f786a69e4710e2aedaa323155c821
komed3 <webmaster@komed3.de>
2026-09-09 14:02:49 +0200

    declare DMS class

--- src/dms/DMS.ts
+++ src/dms/DMS.ts
@@ -1,9 +1,17 @@
 export type TDirection = 'north' | 'east' | 'south' | 'west';
 
-export interface TAngleStringOptions {
+export interface TDMSStringOptions {
   locale?: string;
   precision?: number;
   showUnit?: boolean;
   lang?: Partial< Record< TDirection, string > >;
   delimiter?: string;
+}
+
+
+export class DMS {
+  public readonly degrees: number;
+  public readonly minutes: number;
+  public readonly seconds: number;
+  public readonly direction: TDirection | null;
 }
