3f16e39a673bce7fab2f6a3d5b49112068a208e5
komed3 <webmaster@komed3.de>
2026-09-09 11:07:55 +0200

    update LongitudeRange.ts

--- src/coord/range/LongitudeRange.ts
+++ src/coord/range/LongitudeRange.ts
@@ -3,6 +3,6 @@ import { Range } from '../../base/Range';
 
 export class LongitudeRange extends Range {
   public constructor () {
-    super( { min: -180, max: 180 } );
+    super( { min: -180, max: 180, maxInclusive: false } );
   }
 }
