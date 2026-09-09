442ba045ed856e3dcb5ebac5e929da98dbbf9240
komed3 <webmaster@komed3.de>
2026-09-09 10:06:56 +0200

    update LatitudeRange.ts and LongitudeRange.ts

--- src/coord/LatitudeRange.ts
+++ src/coord/LatitudeRange.ts
@@ -2,5 +2,7 @@ import { Range } from '../base/Range';
 
 
 export class LatitudeRange extends Range {
-  public constructor () { super( { min: -90, max: 90 } ) }
+  public constructor () {
+    super( { min: -90, max: 90 } );
+  }
 }
--- src/coord/LongitudeRange.ts
+++ src/coord/LongitudeRange.ts
@@ -2,5 +2,7 @@ import { Range } from '../base/Range';
 
 
 export class LongitudeRange extends Range {
-  public constructor () { super( { min: -180, max: 180 } ) }
+  public constructor () {
+    super( { min: -180, max: 180 } );
+  }
 }
