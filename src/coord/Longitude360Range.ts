c6c958ba44f29a45c858053470d6bd100990249e
komed3 <webmaster@komed3.de>
2026-09-09 10:06:53 +0200

    feat: create Longitude360Range.ts

---
+++ src/coord/Longitude360Range.ts
@@ -0,0 +1,8 @@
+import { Range } from '../base/Range';
+
+
+export class Longitude360Range extends Range {
+  public constructor () {
+    super( { min: 0, max: 360, maxInclusive: false } );
+  }
+}
