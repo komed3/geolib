cfae8cfbad63d360761a9f6274f7d9f96ae42794
komed3 <webmaster@komed3.de>
2026-09-09 10:11:04 +0200

    feat: create GeocentricXAxis.ts

---
+++ src/coord/GeocentricXAxis.ts
@@ -0,0 +1,16 @@
+import { Axis } from '../base/Axis';
+import { Range } from '../base/Range';
+import { METRE } from '../lib/Units';
+
+
+export class GeocentricXAxis extends Axis {
+  public constructor () {
+    super( {
+      name: 'Geocentric X',
+      direction: 'geocentricX',
+      unit: METRE,
+      range: new Range(),
+      behavior: 'none'
+    } );
+  }
+}
