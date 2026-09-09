4f1330170eff9eb9b84664b901c6233de930b81b
komed3 <webmaster@komed3.de>
2026-09-09 14:26:46 +0200

    update DMS types

--- src/dms/Angle.ts
+++ src/dms/Angle.ts
@@ -1,13 +1,16 @@
 export type TDirection = 'north' | 'east' | 'south' | 'west';
 
+export type TNotation = 'signed' | 'directional';
+
 export type TDirectionMap = Partial< Record< TDirection, string > >;
 
 export interface TAngleStringOptions {
   locale?: string;
   precision?: number;
-  showUnit?: boolean;
-  lang?: TDirectionMap;
   delimiter?: string;
+  showUnit?: boolean;
+  dirMap?: TDirectionMap;
+  notation?: TNotation;
 }
 
 
