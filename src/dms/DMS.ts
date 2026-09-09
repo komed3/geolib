b76956afd1e826816bd4eaeab4e88a5b5f8fe201
komed3 <webmaster@komed3.de>
2026-09-09 14:15:05 +0200

    update DMS.ts

--- src/dms/DMS.ts
+++ src/dms/DMS.ts
@@ -14,4 +14,17 @@ export class DMS {
       Math.abs( this.degrees ) + this.minutes / 60 + this.seconds / 3600
     );
   }
+
+  public equals ( { degrees, minutes, seconds, direction }: DMS ) : boolean {
+    return this.degrees === degrees && this.minutes === minutes &&
+      this.seconds === seconds && this.direction === direction;
+  }
+
+  public clone () : DMS {
+    return new DMS( this.degrees, this.minutes, this.seconds, this.direction );
+  }
+
+  public toJSON () : { degrees: number, minutes: number, seconds: number, direction: TDirection | null } {
+    return { degrees: this.degrees, minutes: this.minutes, seconds: this.seconds, direction: this.direction };
+  }
 }
