917c93d4d89a804776407281af6679ee2d929d65
komed3 <webmaster@komed3.de>
2026-09-09 23:18:47 +0200

    declare direction maps

--- src/base/DMS.ts
+++ src/base/DMS.ts
@@ -22,6 +22,12 @@ export interface TDMSStringOptions {
 }
 
 
+export const DIRECTION_MAP_EN: TDirectionMap = { north: 'N', east: 'E', south: 'S', west: 'W' };
+export const DIRECTION_MAP_DE: TDirectionMap = { north: 'N', east: 'O', south: 'S', west: 'W' };
+
+const DMS_REGEX = /^\s*([+-]?\d+(?:\.\d+)?)(?:\s*°)?(?:\s*(\d+(?:\.\d+)?))?(?:\s*[′'])?(?:\s*(\d+(?:\.\d+)?))?(?:\s*[″"])?(?:\s*([NSEW]))?\s*$/i;
+
+
 export class DMS {
   public constructor () {}
 }
