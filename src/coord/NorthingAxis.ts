9e8f473e997bb05a1315eb6954f1b6ff05c301c8
komed3 <webmaster@komed3.de>
2026-09-09 10:10:25 +0200

    feat: create NorthingAxis.ts

---
+++ src/coord/NorthingAxis.ts
@@ -0,0 +1,16 @@
+import { Axis } from '../base/Axis';
+import { Range } from '../base/Range';
+import { METRE } from '../lib/Units';
+
+
+export class NorthingAxis extends Axis {
+  public constructor () {
+    super( {
+      name: 'Northing',
+      direction: 'north',
+      unit: METRE,
+      range: new Range(),
+      behavior: 'none'
+    } );
+  }
+}
