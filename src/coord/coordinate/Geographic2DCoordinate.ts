595b145a3b198e66ae3671acdb01d7639e4cc321
komed3 <webmaster@komed3.de>
2026-09-09 10:35:44 +0200

    feat: create Geographic2DCoordinate.ts

---
+++ src/coord/coordinate/Geographic2DCoordinate.ts
@@ -0,0 +1,26 @@
+import { Coordinate } from '../../base/Coordinate';
+import type { System } from '../../base/System';
+import { Geographic2DSystem } from '../system/Geographic2DSystem';
+import { Latitude } from '../value/Latitude';
+import { Longitude } from '../value/Longitude';
+
+
+export class Geographic2DCoordinate extends Coordinate {
+  public constructor (
+    longitude: number | Longitude, latitude: number | Latitude,
+    system: System = new Geographic2DSystem()
+  ) {
+    super( system, [
+      longitude instanceof Longitude ? longitude : new Longitude( longitude ),
+      latitude instanceof Latitude ? latitude : new Latitude( latitude )
+    ] );
+  }
+
+  public get longitude () : Longitude {
+    return this.values[ 0 ] as Longitude;
+  }
+
+  public get latitude () : Latitude {
+    return this.values[ 1 ] as Latitude;
+  }
+}
