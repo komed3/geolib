14e7dde419c01767271acd10f53d6b434b3b41f5
komed3 <webmaster@komed3.de>
2026-09-09 14:05:46 +0200

    update DMS.ts

--- src/dms/DMS.ts
+++ src/dms/DMS.ts
@@ -1,17 +1,11 @@
-export type TDirection = 'north' | 'east' | 'south' | 'west';
-
-export interface TDMSStringOptions {
-  locale?: string;
-  precision?: number;
-  showUnit?: boolean;
-  lang?: Partial< Record< TDirection, string > >;
-  delimiter?: string;
-}
+import { type TDirection } from './Angle';
 
 
 export class DMS {
-  public readonly degrees: number;
-  public readonly minutes: number;
-  public readonly seconds: number;
-  public readonly direction: TDirection | null;
+  public constructor (
+    public readonly degrees: number,
+    public readonly minutes: number,
+    public readonly seconds: number,
+    public readonly direction: TDirection | null = null
+  ) {}
 }
