66b8c7a60308f4a818917b969c070cdf2ffcc9af
komed3 <webmaster@komed3.de>
2026-09-10 20:04:49 +0200

    feat: create src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { Axis } from './base/Axis';
+
+
+export type { TAxisBehavior, TAxisOptions, TAxisStringOptions } from './base/Axis';
+
+
+export const geolib = {
+  Axis
+} as const;
+
+export default geolib;
