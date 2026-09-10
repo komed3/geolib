37065c3cd290a98cec743ad326e488d92e8612db
komed3 <webmaster@komed3.de>
2026-09-10 19:34:26 +0200

    supports open boundaries

--- test/base/Range.test.ts
+++ test/base/Range.test.ts
@@ -11,4 +11,13 @@ describe( 'Range', () => {
     expect( range.minInclusive ).toBe( true );
     expect( range.maxInclusive ).toBe( true );
   } );
+
+  it( 'supports open boundaries', () => {
+    const range = new Range( { min: 10, max: 20, minInclusive: false, maxInclusive: false } );
+
+    expect( range.contains( 10 ) ).toBe( false );
+    expect( range.contains( 10.1 ) ).toBe( true );
+    expect( range.contains( 20 ) ).toBe( false );
+    expect( range.contains( 19.9 ) ).toBe( true );
+  } );
 } );
