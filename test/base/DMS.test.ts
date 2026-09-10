ac1bfb85665cbee3d21924f326cd7974cce9948c
komed3 <webmaster@komed3.de>
2026-09-10 02:00:59 +0200

    test: update DMS.test.ts

--- test/base/DMS.test.ts
+++ test/base/DMS.test.ts
@@ -4,10 +4,22 @@ import { DMS, DIRECTION_MAP_DE, type TDirection } from '../../src/base/DMS';
 
 const expectDMS = (
   dms: DMS, degrees: number, minutes: number,
-  seconds: number, direction: TDirection
+  seconds: number, direction: TDirection | null
 ) => {
   expect( dms.degrees ).toBe( degrees );
   expect( dms.minutes ).toBe( minutes );
   expect( dms.seconds ).toBeCloseTo( seconds );
   expect( dms.direction ).toBe( direction );
 };
+
+
+describe( 'DMS', () => {
+  describe( 'constructor', () => {
+    it( 'creates a decimal degree value', () => {
+      const dms = new DMS( 52.5 );
+
+      expectDMS( dms, 52, 30, 0, null );
+      expect( dms.toDecimals() ).toBeCloseTo( 52.5 );
+    } );
+  } );
+} );
