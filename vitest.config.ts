82ed45c8ad5b41c4e093961e51da4b3b6a647e63
komed3 <webmaster@komed3.de>
2026-09-10 16:37:36 +0200

    update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,8 @@
 import { defineConfig } from 'vitest/config';
 
 
-export default defineConfig( { test: {} } );
+export default defineConfig( { test: {
+  include: [ './test/**/*.test.ts' ],
+  logHeapUsage: true,
+  typecheck: { enabled: true }
+} } );
