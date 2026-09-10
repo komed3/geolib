5adb5863794f1dd76daf54e584b715aa2544c460
komed3 <webmaster@komed3.de>
2026-09-10 19:59:38 +0200

    accesses and finds axes

--- test/base/System.test.ts
+++ test/base/System.test.ts
@@ -19,4 +19,13 @@ describe( 'System', () => {
     expect( system.axes ).toBe( axes );
     expect( system.dimension ).toBe( 2 );
   } );
+
+  it( 'accesses and finds axes', () => {
+    const system = new System( 'WGS84', axes );
+
+    expect( system.get( 0 ) ).toBe( longitude );
+    expect( system.get( 1 ) ).toBe( latitude );
+    expect( system.indexOf( latitude.clone() ) ).toBe( 1 );
+    expect( system.indexOf( new Axis( { name: 'height', direction: 'up', unit, range: new Range() } ) ) ).toBe( -1 );
+  } );
 } );
