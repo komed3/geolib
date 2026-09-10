c021985886e5f7fc14db3285684d69ee14ee7992
komed3 <webmaster@komed3.de>
2026-09-10 13:24:51 +0200

    test: update DMS.test.ts

--- test/base/DMS.test.ts
+++ test/base/DMS.test.ts
@@ -202,4 +202,51 @@ describe( 'DMS', () => {
       expect( dms.value ).toBeCloseTo( -52.50416666666667 );
     } );
   } );
+
+  describe( 'fromLongitude', () => {
+    it( 'creates an east longitude from a positive value', () => {
+      const dms = DMS.fromLongitude( 52.50416666666667 );
+
+      expectDMS( dms, 52, 30, 15, 'east' );
+      expect( dms.value ).toBeCloseTo( 52.50416666666667 );
+    } );
+
+    it( 'creates a west longitude from a negative value', () => {
+      const dms = DMS.fromLongitude( -52.50416666666667 );
+
+      expectDMS( dms, 52, 30, 15, 'west' );
+      expect( dms.value ).toBeCloseTo( -52.50416666666667 );
+    } );
+
+    it( 'creates east for zero', () => {
+      const dms = DMS.fromLongitude( 0 );
+
+      expectDMS( dms, 0, 0, 0, 'east' );
+      expect( dms.value ).toBeCloseTo( 0 );
+    } );
+  } );
+
+
+  describe( 'fromLatitude', () => {
+    it( 'creates a north latitude from a positive value', () => {
+      const dms = DMS.fromLatitude( 52.50416666666667 );
+
+      expectDMS( dms, 52, 30, 15, 'north' );
+      expect( dms.value ).toBeCloseTo( 52.50416666666667 );
+    } );
+
+    it( 'creates a south latitude from a negative value', () => {
+      const dms = DMS.fromLatitude( -52.50416666666667 );
+
+      expectDMS( dms, 52, 30, 15, 'south' );
+      expect( dms.value ).toBeCloseTo( -52.50416666666667 );
+    } );
+
+    it( 'creates north for zero', () => {
+      const dms = DMS.fromLatitude( 0 );
+
+      expectDMS( dms, 0, 0, 0, 'north' );
+      expect( dms.value ).toBeCloseTo( 0 );
+    } );
+  } );
 } );
