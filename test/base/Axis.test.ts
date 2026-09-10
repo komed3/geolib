75e896a07c58ef04f2d678f893e4ece11bca0371
komed3 <webmaster@komed3.de>
2026-09-10 19:41:45 +0200

    compares axes by their properties

--- test/base/Axis.test.ts
+++ test/base/Axis.test.ts
@@ -49,4 +49,15 @@ describe( 'Axis', () => {
 
     expect( axis.initialize( 200 ) ).toBe( 200 );
   } );
+
+  it( 'compares axes by their properties', () => {
+    const axis = createAxis( 'clamp' );
+
+    expect( axis.equals( createAxis( 'clamp' ) ) ).toBe( true );
+    expect( axis.equals( createAxis( 'none' ) ) ).toBe( false );
+
+    expect( axis.equals( new Axis( {
+      name: 'latitude', direction: 'north', unit, range, behavior: 'clamp'
+    } ) ) ).toBe( false );
+  } );
 } );
