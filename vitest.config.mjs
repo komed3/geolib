04f108539cd4da906e744603078eaf474810ad31
komed3 <webmaster@komed3.de>
2026-09-10 22:01:57 +0200

    update vitest.config.mjs

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig( { test: {
   include: [ './test/**/*.test.ts' ],
-  logHeapUsage: true,
-  typecheck: { enabled: true }
+  logHeapUsage: true
 } } );
