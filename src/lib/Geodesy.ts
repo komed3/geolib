a097d08bc8d57f64cc1df132ada595f7822002d6
komed3 <webmaster@komed3.de>
2026-09-10 23:39:45 +0200

    update lib

--- src/lib/Geodesy.ts
+++
@@ -1,9 +0,0 @@
-export class Geodesy {
-  public static deg2rad ( deg: number ) : number {
-    return deg * Math.PI / 180;
-  }
-
-  public static rad2deg ( rad: number ) : number {
-    return rad * 180 / Math.PI;
-  }
-}
---
+++ src/lib/geodesy.ts
@@ -0,0 +1,2 @@
+export const deg2rad = ( deg: number ) : number => deg * Math.PI / 180;
+export const rad2deg = ( rad: number ) : number => rad * 180 / Math.PI;
