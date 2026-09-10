c32d4e98f975aa84911c24493f2d0e9840ee2237
komed3 <webmaster@komed3.de>
2026-09-10 19:39:51 +0200

    creates an axis with default behavior

--- test/base/Axis.test.ts
+++ test/base/Axis.test.ts
@@ -9,3 +9,16 @@ const range = new Range( { min: -180, max: 180 } );
 const createAxis = ( behavior?: 'none' | 'clamp' | 'wrap' ) => new Axis( {
   name: 'longitude', direction: 'east', unit, range, behavior
 } );
+
+
+describe( 'Axis', () => {
+  it( 'creates an axis with default behavior', () => {
+    const axis = createAxis();
+
+    expect( axis.name ).toBe( 'longitude' );
+    expect( axis.direction ).toBe( 'east' );
+    expect( axis.unit ).toBe( unit );
+    expect( axis.range ).toBe( range );
+    expect( axis.behavior ).toBe( 'none' );
+  } );
+} );
