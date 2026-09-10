81cd3138fada5922d272b96eb4511b8abfea09b3
komed3 <webmaster@komed3.de>
2026-09-10 19:55:58 +0200

    compare / clone coordinates

--- test/base/Coordinate.test.ts
+++ test/base/Coordinate.test.ts
@@ -27,4 +27,21 @@ describe( 'Coordinate', () => {
 
     expect( coordinate.get( 0 ) ).toBe( values[ 1 ] );
   } );
+
+  it( 'compares coordinates', () => {
+    const coordinate = Coordinate.fromTuple( [ 12.5, 52.5 ], system );
+
+    expect( coordinate.equals( Coordinate.fromTuple( [ 12.5, 52.5 ], system.clone() ) ) ).toBe( true );
+    expect( coordinate.equals( Coordinate.fromTuple( [ 13.5, 52.5 ], system ) ) ).toBe( false );
+  } );
+
+  it( 'clones system and values independently', () => {
+    const coordinate = Coordinate.fromTuple( [ 12.5, 52.5 ], system );
+    const clone = coordinate.clone();
+
+    expect( clone ).not.toBe( coordinate );
+    expect( clone.system ).not.toBe( system );
+    expect( clone.get( 0 ) ).not.toBe( coordinate.get( 0 ) );
+    expect( clone.equals( coordinate ) ).toBe( true );
+  } );
 } );
