77353eafc1fb027e20601728209c66cccb583004
komed3 <webmaster@komed3.de>
2026-09-11 20:00:44 +0200

    update src/unit/index.ts

--- src/unit/index.ts
+++ src/unit/index.ts
@@ -4,3 +4,9 @@ export { Unit } from './Unit';
 
 export { Degree } from './angle/Degree';
 export { Radian } from './angle/Radian';
+
+export { Centimetre } from './length/Centimetre';
+export { Foot } from './length/Foot';
+export { Kilometre } from './length/Kilometre';
+export { Metre } from './length/Metre';
+export { USSurveyFoot } from './length/USSurveyFoot';
