ee068892dd896df8737d16b9a84928a5afa15501
komed3 <webmaster@komed3.de>
2026-09-11 20:17:03 +0200

    update src/unit/index.ts

--- src/unit/index.ts
+++ src/unit/index.ts
@@ -1,4 +1,4 @@
-export type { UnitOptions, UnitStringOptions } from './Unit';
+export type { Quantity, UnitOptions, UnitStringOptions } from './Unit';
 
 export { Unit } from './Unit';
 
@@ -9,8 +9,11 @@ export { Centimetre } from './length/Centimetre';
 export { Foot } from './length/Foot';
 export { Kilometre } from './length/Kilometre';
 export { Metre } from './length/Metre';
+export { Mile } from './length/Mile';
 export { Millimetre } from './length/Millimetre';
+export { NauticalMile } from './length/NauticalMile';
 export { USSurveyFoot } from './length/USSurveyFoot';
+export { Yard } from './length/Yard';
 
 export { Unity } from './scale/Unity';
 
