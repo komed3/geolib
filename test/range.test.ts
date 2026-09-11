c4b8904ed685e2436fd0bbf0afd862f109e4f842
komed3 <webmaster@komed3.de>
2026-09-11 23:26:54 +0200

    test: update range.test.ts

--- test/range.test.ts
+++ test/range.test.ts
@@ -12,4 +12,19 @@ describe( 'Range', () => {
     expect( range.minInclusive ).toBe( true );
     expect( range.maxInclusive ).toBe( true );
   } );
+
+  it( 'supports open boundaries', () => {
+    const range = new Range( { min: 10, max: 20, minInclusive: false, maxInclusive: false } );
+
+    expect( range.contains( 10 ) ).toBe( false );
+    expect( range.contains( 10.1 ) ).toBe( true );
+    expect( range.contains( 20 ) ).toBe( false );
+    expect( range.contains( 19.9 ) ).toBe( true );
+  } );
+
+  it( 'supports unbounded ranges', () => {
+    expect( new Range().contains( -Infinity ) ).toBe( true );
+    expect( new Range( { min: 10 } ).contains( 100 ) ).toBe( true );
+    expect( new Range( { max: 10 } ).contains( -100 ) ).toBe( true );
+  } );
 } );
