dbe5bddbd099ac153e3554100e027145b0bfbf09
komed3 <webmaster@komed3.de>
2026-09-11 19:07:52 +0200

    test: update Unit.ts

--- src/unit/Unit.ts
+++ src/unit/Unit.ts
@@ -1,14 +1,18 @@
+export type Quantity = 'angle' | 'scale' | 'length' | 'time';
+
 export interface UnitOptions {
   name: string;
   unit: string;
+  quantity: Quantity;
 }
 
 
 export class Unit {
   public readonly name: string;
   public readonly unit: string;
+  public readonly quantity: Quantity;
 
-  public constructor ( { name, unit }: UnitOptions ) {
-    this.name = name, this.unit = unit;
+  public constructor ( { name, unit, quantity }: UnitOptions ) {
+    this.name = name, this.unit = unit, this.quantity = quantity;
   }
 }
