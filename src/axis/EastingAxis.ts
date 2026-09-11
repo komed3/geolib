0ecdf3863952070ef6cb47e183d8a00971f157a6
komed3 <webmaster@komed3.de>
2026-09-11 23:09:09 +0200

    feat: create EastingAxis.ts and NorthingAxis.ts

--- src/axis/EastingAxis.ts
+++ src/axis/EastingAxis.ts
@@ -3,8 +3,8 @@ import { Metre, type Unit } from '../unit';
 import { Axis } from './Axis';
 
 
-export class HeightAxis extends Axis {
+export class EastingAxis extends Axis {
   public constructor ( unit: Unit = new Metre(), range: Range = new Range() ) {
-    super( { name: 'Height', orientation: 'up', unit, range, behavior: 'none' } );
+    super( { name: 'Easting', orientation: 'east', unit, range, behavior: 'none' } );
   }
 }
