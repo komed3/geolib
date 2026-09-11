7cc1408d3a66bdbdf61574e4f364b331d98e9b36
komed3 <webmaster@komed3.de>
2026-09-11 19:00:52 +0200

    feat: create src/range/index.ts

---
+++ src/range/index.ts
@@ -0,0 +1,6 @@
+export type { RangeOptions, RangeStringOptions } from './Range';
+
+export { LatitudeRange } from './LatitudeRange';
+export { Longitude360Range } from './Longitude360Range';
+export { LongitudeRange } from './LongitudeRange';
+export { Range } from './Range';
