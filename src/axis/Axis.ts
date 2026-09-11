9411b4f16eab95799242ebcf3c0bbfecf03e422c
komed3 <webmaster@komed3.de>
2026-09-11 22:44:57 +0200

    update Axis.ts

--- src/axis/Axis.ts
+++ src/axis/Axis.ts
@@ -1,3 +1,18 @@
+import { Range } from '../range';
+import { Unit } from '../unit';
+
+
+export type AxisBehavior = 'none' | 'clamp' | 'wrap';
+
+export interface AxisOptions {
+  name: string;
+  orientation: string;
+  unit: Unit;
+  range: Range;
+  behavior?: AxisBehavior;
+  abbr?: string;
+}
+
 export interface AxisStringOptions {
   displayUnit?: boolean;
 }
