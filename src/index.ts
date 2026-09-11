feeebb50c3a864abfd02f1303aa26ac1511012a5
komed3 <webmaster@komed3.de>
2026-09-11 11:38:24 +0200

    update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -22,8 +22,12 @@ import { DMS, DMS_DIRMAP_DE, DMS_DIRMAP_EN, DMS_UNITS } from './dms/DMS';
 
 import { Airy1830Ellipsoid } from './ellipsoid/Airy1830Ellipsoid';
 import { Airy1849Ellipsoid } from './ellipsoid/Airy1849Ellipsoid';
-import { AustralianNationalSpheroidEllipsoid } from './ellipsoid/AustralianNationalSpheroidEllipsoid';
+import { ANSEllipsoid } from './ellipsoid/ANSEllipsoid';
+import { Bessel1841Ellipsoid } from './ellipsoid/Bessel1841Ellipsoid';
+import { BesselModifiedEllipsoid } from './ellipsoid/BesselModifiedEllipsoid';
 import { CGCS2000Ellipsoid } from './ellipsoid/CGCS2000Ellipsoid';
+import { Clarke1858Ellipsoid } from './ellipsoid/Clarke1858Ellipsoid';
+import { Clarke1866Ellipsoid } from './ellipsoid/Clarke1866Ellipsoid';
 import { Ellipsoid } from './ellipsoid/Ellipsoid';
 import { GSK2011Ellipsoid } from './ellipsoid/GSK2011Ellipsoid';
 import { WGS84Ellipsoid } from './ellipsoid/WGS84Ellipsoid';
@@ -71,9 +75,10 @@ export type { TValueStringOptions } from './value/Value';
 
 
 export {
-  Airy1830Ellipsoid, Airy1849Ellipsoid, Altitude, AltitudeAxis, AustralianNationalSpheroidEllipsoid,
-  Axis, AxisSet, CGCS2000Ellipsoid, Coordinate, DegreeValue, DMS, DMS_DIRMAP_DE, DMS_DIRMAP_EN,
-  DMS_UNITS, Easting, EastingAxis, Ellipsoid, GeocentricCoordinate, GeocentricSystem, GeocentricX,
+  Airy1830Ellipsoid, Airy1849Ellipsoid, Altitude, AltitudeAxis, ANSEllipsoid, Axis, AxisSet,
+  Bessel1841Ellipsoid, BesselModifiedEllipsoid, CGCS2000Ellipsoid, Clarke1858Ellipsoid,
+  Clarke1866Ellipsoid, Coordinate, DegreeValue, DMS, DMS_DIRMAP_DE, DMS_DIRMAP_EN, DMS_UNITS,
+  Easting, EastingAxis, Ellipsoid, GeocentricCoordinate, GeocentricSystem, GeocentricX,
   GeocentricXAxis, GeocentricY, GeocentricYAxis, GeocentricZ, GeocentricZAxis, geodesy,
   Geographic2DCoordinate, Geographic2DSystem, Geographic3DCoordinate, Geographic3DSystem,
   GSK2011Ellipsoid, Height, HeightAxis, Latitude, LatitudeAxis, LatitudeRange, Longitude,
@@ -84,9 +89,10 @@ export {
 
 
 export const geolib = {
-  Airy1830Ellipsoid, Airy1849Ellipsoid, Altitude, AltitudeAxis, AustralianNationalSpheroidEllipsoid,
-  Axis, AxisSet, CGCS2000Ellipsoid, Coordinate, DegreeValue, DMS, DMS_DIRMAP_DE, DMS_DIRMAP_EN,
-  DMS_UNITS, Easting, EastingAxis, Ellipsoid, GeocentricCoordinate, GeocentricSystem, GeocentricX,
+  Airy1830Ellipsoid, Airy1849Ellipsoid, Altitude, AltitudeAxis, ANSEllipsoid, Axis, AxisSet,
+  Bessel1841Ellipsoid, BesselModifiedEllipsoid, CGCS2000Ellipsoid, Clarke1858Ellipsoid,
+  Clarke1866Ellipsoid, Coordinate, DegreeValue, DMS, DMS_DIRMAP_DE, DMS_DIRMAP_EN, DMS_UNITS,
+  Easting, EastingAxis, Ellipsoid, GeocentricCoordinate, GeocentricSystem, GeocentricX,
   GeocentricXAxis, GeocentricY, GeocentricYAxis, GeocentricZ, GeocentricZAxis, geodesy,
   Geographic2DCoordinate, Geographic2DSystem, Geographic3DCoordinate, Geographic3DSystem,
   GSK2011Ellipsoid, Height, HeightAxis, Latitude, LatitudeAxis, LatitudeRange, Longitude,
