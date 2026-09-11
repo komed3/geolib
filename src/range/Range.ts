95be371aaa0c4060a348ccd29175a33fc10e30a0
komed3 <webmaster@komed3.de>
2026-09-11 18:47:50 +0200

    feat: create src/index.ts and Range.ts

---
+++ src/range/Range.ts
@@ -0,0 +1,6 @@
+export class Range {
+  public readonly min: number | null;
+  public readonly max: number | null;
+  public readonly minInclusive: boolean;
+  public readonly maxInclusive: boolean;
+}
