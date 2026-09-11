220a9e298564e87e48501b60265fea3403c1ea09
komed3 <webmaster@komed3.de>
2026-09-12 00:03:47 +0200

    clone axis set tests

--- test/axis.test.ts
+++ test/axis.test.ts
@@ -6,7 +6,7 @@ import { Unit } from '../src/unit';
 
 const latitude = new LatitudeAxis(), longitude = new LongitudeAxis();
 const unit = new Unit( { name: 'metre', unit: 'm', quantity: 'length' } ), range = new Range();
-const set = new AxisSet( { axes: [ longitude, latitude ] } );
+const axes = new AxisSet( { axes: [ longitude, latitude ] } );
 
 
 describe( 'Axis', () => {
@@ -15,16 +15,30 @@ describe( 'Axis', () => {
 
 describe( 'AxisSet', () => {
   it( 'creates an immutable axis collection', () => {
-    expect( set.dimension ).toBe( 2 );
-    expect( set.get( 0 ) ).toBe( longitude );
-    expect( set.get( 1 ) ).toBe( latitude );
-    expect( set.toArray() ).toEqual( [ longitude, latitude ] );
+    expect( axes.dimension ).toBe( 2 );
+    expect( axes.get( 0 ) ).toBe( longitude );
+    expect( axes.get( 1 ) ).toBe( latitude );
+    expect( axes.toArray() ).toEqual( [ longitude, latitude ] );
   } );
 
   it( 'finds and checks axes by equality', () => {
-    expect( set.indexOf( latitude ) ).toBe( 1 );
-    expect( set.indexOf( longitude.clone() ) ).toBe( 0 );
-    expect( set.indexOf( new Axis( { name: 'height', orientation: 'up', unit, range } ) ) ).toBe( -1 );
-    expect( set.has( latitude.clone() ) ).toBe( true );
+    expect( axes.indexOf( latitude ) ).toBe( 1 );
+    expect( axes.indexOf( longitude.clone() ) ).toBe( 0 );
+    expect( axes.indexOf( new Axis( { name: 'height', orientation: 'up', unit, range } ) ) ).toBe( -1 );
+    expect( axes.has( latitude.clone() ) ).toBe( true );
+  } );
+
+  it( 'compares axis sets', () => {
+    expect( axes.equals( new AxisSet( { axes: [ longitude.clone(), latitude.clone() ] } ) ) ).toBe( true );
+    expect( axes.equals( new AxisSet( { axes: [ latitude, longitude ] } ) ) ).toBe( false );
+    expect( axes.equals( new AxisSet( { axes: [ longitude ] } ) ) ).toBe( false );
+  } );
+
+  it( 'clones all axes independently', () => {
+    const clone = axes.clone();
+
+    expect( clone ).not.toBe( axes );
+    expect( clone.get( 0 ) ).not.toBe( longitude );
+    expect( clone.equals( axes ) ).toBe( true );
   } );
 } );
