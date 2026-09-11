d5c29b34eaa26b954bc18328742b0b5af3bd7580
komed3 <webmaster@komed3.de>
2026-09-11 19:59:52 +0200

    test: create Centimetre.ts

---
+++ src/unit/length/Centimetre.ts
@@ -0,0 +1,8 @@
+import { Unit } from '../Unit';
+
+
+export class Centimetre extends Unit {
+  public constructor () {
+    super( { name: 'Centimetre', unit: 'cm', quantity: 'length', factor: 100 } );
+  }
+}
