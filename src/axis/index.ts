b3bcda87237689a452e60674808134fd434295a4
komed3 <webmaster@komed3.de>
2026-09-11 23:03:25 +0200

    feat: create src/axis/index.ts

---
+++ src/axis/index.ts
@@ -0,0 +1,5 @@
+export type { AxisBehavior, AxisOptions, AxisStringOptions } from './Axis';
+export type { AxisSetOptions, AxisSetStringOptions } from './AxisSet';
+
+export { Axis } from './Axis';
+export { AxisSet } from './AxisSet';
