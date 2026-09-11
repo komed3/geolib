cf67126473c7a54bdb77c1d3a9226e53d15031f2
komed3 <webmaster@komed3.de>
2026-09-11 19:11:30 +0200

    transform unit

--- src/unit/Unit.ts
+++ src/unit/Unit.ts
@@ -17,4 +17,11 @@ export class Unit {
   public constructor ( { name, unit, quantity, factor = 1 }: UnitOptions ) {
     this.name = name, this.unit = unit, this.quantity = quantity, this.factor = factor;
   }
+
+  public transform ( value: number, { quantity, factor }: Unit ) : number {
+    if ( this.quantity !== quantity )
+      throw new Error( `Cannot transform ${ this.quantity } to ${ quantity }` );
+
+    return value * factor / this.factor;
+  }
 }
