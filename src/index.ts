19a485e0793b6b6da43971cd90c53e395cd2eaee
komed3 <webmaster@komed3.de>
2026-09-11 09:59:35 +0200

    feat: create src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,89 @@
+import { AltitudeAxis } from './axis/AltitudeAxis';
+import { Axis } from './axis/Axis';
+import { AxisSet } from './axis/AxisSet';
+import { EastingAxis } from './axis/EastingAxis';
+import { GeocentricXAxis } from './axis/GeocentricXAxis';
+import { GeocentricYAxis } from './axis/GeocentricYAxis';
+import { GeocentricZAxis } from './axis/GeocentricZAxis';
+import { HeightAxis } from './axis/HeightAxis';
+import { LatitudeAxis } from './axis/LatitudeAxis';
+import { Longitude360Axis } from './axis/Longitude360Axis';
+import { LongitudeAxis } from './axis/LongitudeAxis';
+import { NorthingAxis } from './axis/NorthingAxis';
+
+import { Coordinate } from './coord/Coordinate';
+import { GeocentricCoordinate } from './coord/GeocentricCoordinate';
+import { Geographic2DCoordinate } from './coord/Geographic2DCoordinate';
+import { Geographic3DCoordinate } from './coord/Geographic3DCoordinate';
+import { Projected2DCoordinate } from './coord/Projected2DCoordinate';
+import { Projected3DCoordinate } from './coord/Projected3DCoordinate';
+
+import { DMS, DMS_DIRMAP_DE, DMS_DIRMAP_EN, DMS_UNITS } from './dms/DMS';
+
+import * as geodesy from './lib/geodesy';
+import * as units from './lib/units';
+import * as utils from './lib/utils';
+
+import { LatitudeRange } from './range/LatitudeRange';
+import { Longitude360Range } from './range/Longitude360Range';
+import { LongitudeRange } from './range/LongitudeRange';
+import { Range } from './range/Range';
+
+import { GeocentricSystem } from './system/GeocentricSystem';
+import { Geographic2DSystem } from './system/Geographic2DSystem';
+import { Geographic3DSystem } from './system/Geographic3DSystem';
+import { Projected2DSystem } from './system/Projected2DSystem';
+import { Projected3DSystem } from './system/Projected3DSystem';
+import { System } from './system/System';
+
+import { Altitude } from './value/Altitude';
+import { DegreeValue } from './value/DegreeValue';
+import { Easting } from './value/Easting';
+import { GeocentricX } from './value/GeocentricX';
+import { GeocentricY } from './value/GeocentricY';
+import { GeocentricZ } from './value/GeocentricZ';
+import { Height } from './value/Height';
+import { Latitude } from './value/Latitude';
+import { Longitude } from './value/Longitude';
+import { Longitude360 } from './value/Longitude360';
+import { Northing } from './value/Northing';
+import { Value } from './value/Value';
+
+
+export type { TAxisBehavior, TAxisOptions, TAxisStringOptions } from './axis/Axis';
+export type { TAxisSetStringOptions } from './axis/AxisSet';
+export type { TCoordinateStringOptions } from './coord/Coordinate';
+export type { TDirection, TDirectionMap, TDMS, TDMSFormat, TDMSOptions, TDMSStringOptions, TDMSUnits, TNotation } from './dms/DMS';
+export type { TUnit } from './lib/units';
+export type { TRangeOptions, TRangeStringOptions } from './range/Range';
+export type { TSystemStringOptions } from './system/System';
+export type { TValueStringOptions } from './value/Value';
+
+
+export {
+  Altitude, AltitudeAxis, Axis, AxisSet, Coordinate, DegreeValue, DMS, DMS_DIRMAP_DE,
+  DMS_DIRMAP_EN, DMS_UNITS, Easting, EastingAxis, GeocentricCoordinate, GeocentricSystem,
+  GeocentricX, GeocentricXAxis, GeocentricY, GeocentricYAxis, GeocentricZ, GeocentricZAxis,
+  geodesy, Geographic2DCoordinate, Geographic2DSystem, Geographic3DCoordinate,
+  Geographic3DSystem, Height, HeightAxis, Latitude, LatitudeAxis, LatitudeRange, Longitude,
+  Longitude360, Longitude360Axis, Longitude360Range, LongitudeAxis, LongitudeRange,
+  Northing, NorthingAxis, Projected2DCoordinate, Projected2DSystem, Projected3DCoordinate,
+  Projected3DSystem, Range, System,
+  units, utils, Value
+};
+
+
+export const geolib = {
+  Altitude, AltitudeAxis, Axis, AxisSet, Coordinate, DegreeValue, DMS, DMS_DIRMAP_DE,
+  DMS_DIRMAP_EN, DMS_UNITS, Easting, EastingAxis, GeocentricCoordinate, GeocentricSystem,
+  GeocentricX, GeocentricXAxis, GeocentricY, GeocentricYAxis, GeocentricZ, GeocentricZAxis,
+  geodesy, Geographic2DCoordinate, Geographic2DSystem, Geographic3DCoordinate,
+  Geographic3DSystem, Height, HeightAxis, Latitude, LatitudeAxis, LatitudeRange, Longitude,
+  Longitude360, Longitude360Axis, Longitude360Range, LongitudeAxis, LongitudeRange,
+  Northing, NorthingAxis, Projected2DCoordinate, Projected2DSystem, Projected3DCoordinate,
+  Projected3DSystem, Range, System,
+  units, utils, Value
+} as const;
+
+
+export default geolib;
