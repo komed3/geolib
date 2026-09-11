b9d73d286e44094d7449c50df8689e3039cb3d9e
komed3 <webmaster@komed3.de>
2026-09-11 20:21:10 +0200

    bug fix

--- src/unit/angle/Degree.ts
+++ src/unit/angle/Degree.ts
@@ -3,6 +3,6 @@ import { Unit } from '../Unit';
 
 export class Degree extends Unit {
   public constructor () {
-    super( { name: 'degree', unit: '°', quantity: 'angle', factor: Math.PI / 180 } );
+    super( { name: 'degree', unit: '°', quantity: 'angle', factor: 180 / Math.PI } );
   }
 }
