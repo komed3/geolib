6fcd02245318f5b420be89acba982b7c498be1bc
komed3 <webmaster@komed3.de>
2026-09-11 10:29:18 +0200

    declare the ellipsoid cls

--- src/ellipsoid/Ellipsoid.ts
+++ src/ellipsoid/Ellipsoid.ts
@@ -7,3 +7,22 @@ export interface TEllipsoidOptions {
 export interface TEllipsoidStringOptions {
   precision?: number;
 }
+
+
+export class Ellipsoid {
+  public readonly name: string;
+  public readonly semiMajorAxis: number;
+  public readonly inverseFlattening: number;
+
+  public constructor ( { name, semiMajorAxis, inverseFlattening }: TEllipsoidOptions ) {
+    if ( ! Number.isFinite( semiMajorAxis ) || semiMajorAxis <= 0 )
+      throw new RangeError( 'Semi-major axis must be a positive finite number' );
+
+    if ( inverseFlattening < 0 || Number.isNaN( inverseFlattening ) )
+      throw new RangeError( 'Inverse flattening must be non-negative' );
+
+    this.name = name;
+    this.semiMajorAxis = semiMajorAxis;
+    this.inverseFlattening = inverseFlattening;
+  }
+}
