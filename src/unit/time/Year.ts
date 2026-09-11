8aeb01bfe99313fb2960c3d54d699bb5ea274bea
komed3 <webmaster@komed3.de>
2026-09-11 23:53:55 +0200

    update Year.ts

--- src/unit/time/Year.ts
+++ src/unit/time/Year.ts
@@ -3,6 +3,6 @@ import { Unit } from '../Unit';
 
 export class Year extends Unit {
   public constructor () {
-    super( { name: 'year', unit: 'a', quantity: 'time', factor: 31556952 } );
+    super( { name: 'year', unit: 'a', quantity: 'time', factor: 31558149.54 } );
   }
 }
