500efb3edc12906ba83544cd5c6719f46e4d1123
komed3 <webmaster@komed3.de>
2026-09-11 18:49:22 +0200

    add constructor for range cls

--- src/range/Range.ts
+++ src/range/Range.ts
@@ -1,6 +1,18 @@
+export interface TRangeOptions {
+  min?: number | null;
+  max?: number | null;
+  minInclusive?: boolean;
+  maxInclusive?: boolean;
+}
+
+
 export class Range {
   public readonly min: number | null;
   public readonly max: number | null;
   public readonly minInclusive: boolean;
   public readonly maxInclusive: boolean;
+
+  public constructor ( { min = null, max = null, minInclusive = true, maxInclusive = true }: TRangeOptions = {} ) {
+    this.min = min, this.max = max, this.minInclusive = minInclusive, this.maxInclusive = maxInclusive;
+  }
 }
