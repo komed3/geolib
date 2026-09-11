a2bf1ccc689a1b2e94019685fb150eb9679c017c
komed3 <webmaster@komed3.de>
2026-09-11 23:18:06 +0200

    update Longitude360Axis.ts and src/axis/index.ts

--- src/axis/Longitude360Axis.ts
+++ src/axis/Longitude360Axis.ts
@@ -3,7 +3,7 @@ import { Degree } from '../unit';
 import { Axis } from './Axis';
 
 
-export class LongitudeAxis extends Axis {
+export class Longitude360Axis extends Axis {
   public constructor () {
     super( {
       name: 'Longitude 360°', orientation: 'east', unit: new Degree(),
--- src/axis/index.ts
+++ src/axis/index.ts
@@ -3,3 +3,13 @@ export type { AxisSetOptions, AxisSetStringOptions } from './AxisSet';
 
 export { Axis } from './Axis';
 export { AxisSet } from './AxisSet';
+
+export { EastingAxis } from './EastingAxis';
+export { GeocentricXAxis } from './GeocentricXAxis';
+export { GeocentricYAxis } from './GeocentricYAxis';
+export { GeocentricZAxis } from './GeocentricZAxis';
+export { HeightAxis } from './HeightAxis';
+export { LatitudeAxis } from './LatitudeAxis';
+export { Longitude360Axis } from './Longitude360Axis';
+export { LongitudeAxis } from './LongitudeAxis';
+export { NorthingAxis } from './NorthingAxis';
