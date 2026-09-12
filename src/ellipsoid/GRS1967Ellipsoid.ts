9f2133444f208d30c0dbaf7f63293ff23f1adea6
komed3 <webmaster@komed3.de>
2026-09-12 11:42:07 +0200

    feat: create GRS1967Ellipsoid.ts, GRS1980Ellipsoid.ts and GSK2011Ellipsoid.ts

---
+++ src/ellipsoid/GRS1967Ellipsoid.ts
@@ -0,0 +1,12 @@
+import { Ellipsoid } from './Ellipsoid';
+
+
+export class GRS1967Ellipsoid extends Ellipsoid {
+  public constructor () {
+    super( {
+      name: 'GRS 1967',
+      semiMajorAxis: 6378160,
+      inverseFlattening: 298.247167427
+    } );
+  }
+}
---
+++ src/ellipsoid/GSK2011Ellipsoid.ts
@@ -0,0 +1,12 @@
+import { Ellipsoid } from './Ellipsoid';
+
+
+export class GSK2011Ellipsoid extends Ellipsoid {
+  public constructor () {
+    super( {
+      name: 'GSK-2011',
+      semiMajorAxis: 6378136.5,
+      inverseFlattening: 298.2564151
+    } );
+  }
+}
