1ccb8362ca67b08e313fe277dd224ba365568bbf
komed3 <webmaster@komed3.de>
2026-09-12 20:30:36 +0200

    update src/coord/index.ts

--- src/coord/index.ts
+++ src/coord/index.ts
@@ -1,3 +1,9 @@
 export type { CoordinateOptions, CoordinateOrthant, CoordinateStringOptions } from './Coordinate';
 
 export { Coordinate } from './Coordinate';
+
+export { GeocentricCoordinate } from './GeocentricCoordinate';
+export { Geographic2DCoordinate } from './Geographic2DCoordinate';
+export { Geographic3DCoordinate } from './Geographic3DCoordinate';
+export { Projected2DCoordinate } from './Projected2DCoordinate';
+export { Projected3DCoordinate } from './Projected3DCoordinate';
