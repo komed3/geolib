bac9b9798f1a21b6b00ce20579969b6c0cd2e2a8
komed3 <webmaster@komed3.de>
2026-09-12 10:04:38 +0200

    degree value to radians

--- src/value/DegreeValue.ts
+++ src/value/DegreeValue.ts
@@ -1,4 +1,9 @@
+import { deg2rad } from '../utils';
 import { Value } from './Value';
 
 
-export class DegreeValue extends Value {}
+export class DegreeValue extends Value {
+  public toRadians () : number {
+    return deg2rad( this.value );
+  }
+}
