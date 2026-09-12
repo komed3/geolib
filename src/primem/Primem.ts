fd28701bdbccc9e486480c39197220a9fe140954
komed3 <webmaster@komed3.de>
2026-09-12 10:27:15 +0200

    declare prime meridian options

--- src/primem/Primem.ts
+++ src/primem/Primem.ts
@@ -1,3 +1,13 @@
+import type { Axis } from '../axis';
+
+
+export interface PrimemOptions {
+  name: string;
+  value: number;
+  axis?: Axis;
+}
+
+
 export class Primem {
   public constructor () {}
 }
