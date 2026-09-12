b369cf38e3986aedf294f20571bba61068eef5b0
komed3 <webmaster@komed3.de>
2026-09-12 10:07:11 +0200

    update exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import * as axis from './axis';
 import * as range from './range';
 import * as unit from './unit';
+import * as value from './value';
 
 export * from './axis';
 export * from './range';
 export * from './unit';
+export * from './value';
 
-export const geolib = { ...axis, ...range, ...unit } as const;
+export { axis, range, unit, value };
+
+export const geolib = { ...axis, ...range, ...unit, ...value } as const;
 export default geolib;
