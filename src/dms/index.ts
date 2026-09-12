b6f45d8af5f44c8e0541e1f65ef5613eba502427
komed3 <webmaster@komed3.de>
2026-09-12 11:22:57 +0200

    feat: create src/dms/index.ts

---
+++ src/dms/index.ts
@@ -0,0 +1,6 @@
+export type {
+  DMSDirection, DMSDirectionMap, DMSFormat, DMSNotation, DMSObject,
+  DMSStringOptions, DMSTuple, DMSUnitMap
+} from './DMS';
+
+export { DMS, DMS_DIRMAP_DE, DMS_DIRMAP_EN, DMS_UNITMAP } from './DMS';
