b6eeb4da13d560abbb302c3f37ee8968c0dbf849
komed3 <webmaster@komed3.de>
2026-09-12 11:23:58 +0200

    export DMS class, consts, types

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import * as axis from './axis';
+import * as dms from './dms';
 import * as primem from './primem';
 import * as range from './range';
 import * as unit from './unit';
 import * as value from './value';
 
 export * from './axis';
+export * from './dms';
 export * from './primem';
 export * from './range';
 export * from './unit';
 export * from './value';
 
-export { axis, primem, range, unit, value };
+export { axis, dms, primem, range, unit, value };
 
-export const geolib = { ...axis, ...primem, ...range, ...unit, ...value } as const;
+export const geolib = { ...axis, ...dms, ...primem, ...range, ...unit, ...value } as const;
 export default geolib;
