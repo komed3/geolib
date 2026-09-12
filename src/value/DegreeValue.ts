475bed23c319e286a7e9d6e0b219f5af521c64f8
komed3 <webmaster@komed3.de>
2026-09-12 10:14:11 +0200

    degree value must have degree axis

--- src/value/DegreeValue.ts
+++ src/value/DegreeValue.ts
@@ -1,8 +1,16 @@
+import type { Axis } from '../axis';
 import { deg2rad } from '../utils';
 import { Value } from './Value';
 
 
 export class DegreeValue extends Value {
+  public constructor ( value: number, axis: Axis ) {
+    if ( axis.unit.name !== 'degree' )
+      throw new Error( `Axis unit must be degree, got ${ axis.unit.name }` );
+
+    super( value, axis );
+  }
+
   public toRadians () : number {
     return deg2rad( this.value );
   }
