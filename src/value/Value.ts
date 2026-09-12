32adf62d64d9b83d6144d451581202b60262ef30
komed3 <webmaster@komed3.de>
2026-09-12 09:55:53 +0200

    update Value.ts

--- src/value/Value.ts
+++ src/value/Value.ts
@@ -1,11 +1,16 @@
 import { Axis } from '../axis';
 
 
+export interface ValueOptions {
+  axis: Axis;
+}
+
+
 export class Value {
   public readonly value: number;
   public readonly axis: Axis;
 
-  public constructor ( value: number, axis: Axis ) {
+  public constructor ( value: number, { axis }: ValueOptions ) {
     this.axis = axis, this.value = axis.normalize( value );
   }
 }
