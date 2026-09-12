1e804184681a51badb3ac2c6aa01f79614d30ae5
komed3 <webmaster@komed3.de>
2026-09-12 11:43:06 +0200

    update src/ellipsoid/index.ts

--- src/ellipsoid/index.ts
+++ src/ellipsoid/index.ts
@@ -1,3 +1,25 @@
 export type { EllipsoidOptions, EllipsoidStringOptions } from './Ellipsoid';
 
 export { Ellipsoid } from './Ellipsoid';
+
+export { Airy1830Ellipsoid } from './Airy1830Ellipsoid';
+export { ANSEllipsoid } from './ANSEllipsoid';
+export { Bessel1841Ellipsoid } from './Bessel1841Ellipsoid';
+export { CGCS2000Ellipsoid } from './CGCS2000Ellipsoid';
+export { Clarke1858Ellipsoid } from './Clarke1858Ellipsoid';
+export { Clarke1866Ellipsoid } from './Clarke1866Ellipsoid';
+export { Clarke1880Ellipsoid } from './Clarke1880Ellipsoid';
+export { Danish1876Ellipsoid } from './Danish1876Ellipsoid';
+export { Everest1830Ellipsoid } from './Everest1830Ellipsoid';
+export { Everest1937Ellipsoid } from './Everest1937Ellipsoid';
+export { GRS1967Ellipsoid } from './GRS1967Ellipsoid';
+export { GRS1980Ellipsoid } from './GRS1980Ellipsoid';
+export { GSK2011Ellipsoid } from './GSK2011Ellipsoid';
+export { Helmert1906Ellipsoid } from './Helmert1906Ellipsoid';
+export { Hough1960Ellipsoid } from './Hough1960Ellipsoid';
+export { International1924Ellipsoid } from './International1924Ellipsoid';
+export { Krassowsky1940Ellipsoid } from './Krassowsky1940Ellipsoid';
+export { PZ90Ellipsoid } from './PZ90Ellipsoid';
+export { Struve1860Ellipsoid } from './Struve1860Ellipsoid';
+export { WGS72Ellipsoid } from './WGS72Ellipsoid';
+export { WGS84Ellipsoid } from './WGS84Ellipsoid';
