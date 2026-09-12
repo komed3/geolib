dff1f2f42d9b23e66ebab2e2840b16a2931d69e9
komed3 <webmaster@komed3.de>
2026-09-12 10:18:46 +0200

    export values

--- src/value/index.ts
+++ src/value/index.ts
@@ -2,3 +2,13 @@ export type { ValueStringOptions } from './Value';
 
 export { DegreeValue } from './DegreeValue';
 export { Value } from './Value';
+
+export { Easting } from './Easting';
+export { GeocentricX } from './GeocentricX';
+export { GeocentricY } from './GeocentricY';
+export { GeocentricZ } from './GeocentricZ';
+export { Height } from './Height';
+export { Latitude } from './Latitude';
+export { Longitude } from './Longitude';
+export { Longitude360 } from './Longitude360';
+export { Northing } from './Northing';
