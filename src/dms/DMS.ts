dc9c8d5465102a4c8d6ec57609fbd2c9eb59d992
komed3 <webmaster@komed3.de>
2026-09-12 11:02:38 +0200

    implement DMS carry logic

--- src/dms/DMS.ts
+++ src/dms/DMS.ts
@@ -1,5 +1,12 @@
 export type Direction = 'north' | 'east' | 'south' | 'west';
 
+export type DMSTuple = [
+  degrees: number,
+  minutes: number,
+  seconds: number,
+  direction?: string
+];
+
 
 export class DMS {
   public readonly value: number;
@@ -7,4 +14,11 @@ export class DMS {
   public readonly degrees: number;
   public readonly minutes: number;
   public readonly seconds: number;
+
+  private carry ( [ degrees, minutes, seconds ]: DMSTuple ) : DMSTuple {
+    minutes += Math.floor( seconds / 60 ), seconds %= 60;
+    degrees += Math.floor( minutes / 60 ), minutes %= 60;
+
+    return [ degrees, minutes, seconds ];
+  }
 }
