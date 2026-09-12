e46f7d182e851b2fa7683d1f714df98de13ee383
komed3 <webmaster@komed3.de>
2026-09-13 01:40:30 +0200

    test: create system.test.ts

---
+++ test/system.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest';
+import { Geographic2DSystem } from '../src/system';
+
+
+const system = new Geographic2DSystem();
+
+
+describe( 'System', () => {} );
