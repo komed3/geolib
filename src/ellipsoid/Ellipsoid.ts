df8a3ecaa5b1261cf03d91d43962d1d9437dbfa2
komed3 <webmaster@komed3.de>
2026-09-12 11:33:00 +0200

    implement getter

--- src/ellipsoid/Ellipsoid.ts
+++ src/ellipsoid/Ellipsoid.ts
@@ -21,4 +21,44 @@ export class Ellipsoid {
     this.semiMajorAxis = semiMajorAxis;
     this.inverseFlattening = inverseFlattening;
   }
+
+  public get flattening () : number {
+    return this.inverseFlattening === Infinity ? 0 : 1 / this.inverseFlattening;
+  }
+
+  public get semiMinorAxis () : number {
+    return this.semiMajorAxis * ( 1 - this.flattening );
+  }
+
+  public get linearEccentricity () : number {
+    return Math.sqrt( this.semiMajorAxis ** 2 - this.semiMinorAxis ** 2 );
+  }
+
+  public get eccentricitySquared () : number {
+    return this.flattening * ( 2 - this.flattening );
+  }
+
+  public get eccentricity () : number {
+    return Math.sqrt( this.eccentricitySquared );
+  }
+
+  public get secondEccentricitySquared () : number {
+    return this.eccentricitySquared / ( 1 - this.eccentricitySquared );
+  }
+
+  public get secondEccentricity () : number {
+    return Math.sqrt( this.secondEccentricitySquared );
+  }
+
+  public get thirdFlattening () : number {
+    return this.flattening / ( 2 - this.flattening );
+  }
+
+   public get volumetricRadius () : number {
+    return ( this.semiMajorAxis ** 2 * this.semiMinorAxis ) ** ( 1 / 3 );
+  }
+
+  public get isSphere () : boolean {
+    return this.flattening === 0;
+  }
 }
