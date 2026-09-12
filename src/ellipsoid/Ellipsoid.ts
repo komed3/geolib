c658eaa8f529ee0e23b177daedae4c019ac91cea
komed3 <webmaster@komed3.de>
2026-09-12 11:32:42 +0200

    describe ellipsoid

--- src/ellipsoid/Ellipsoid.ts
+++ src/ellipsoid/Ellipsoid.ts
@@ -1,5 +1,24 @@
+export interface EllipsoidOptions {
+  name: string;
+  semiMajorAxis: number;
+  inverseFlattening?: number;
+}
+
+
 export class Ellipsoid {
   public readonly name: string;
   public readonly semiMajorAxis: number;
   public readonly inverseFlattening: number;
+
+  public constructor ( { name, semiMajorAxis, inverseFlattening = Infinity }: EllipsoidOptions ) {
+    if ( ! Number.isFinite( semiMajorAxis ) || semiMajorAxis <= 0 )
+      throw new RangeError( 'Semi-major axis must be a positive finite number' );
+
+    if ( Number.isNaN( inverseFlattening ) || ( inverseFlattening <= 0 && inverseFlattening !== Infinity ) )
+      throw new RangeError( 'Inverse flattening must be positive or infinite' );
+
+    this.name = name;
+    this.semiMajorAxis = semiMajorAxis;
+    this.inverseFlattening = inverseFlattening;
+  }
 }
