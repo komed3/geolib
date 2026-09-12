94b74fb89d591116c8a6a37dcd9b4570317abc94
komed3 <webmaster@komed3.de>
2026-09-12 20:29:36 +0200

    feat: create Projected2DCoordinate.ts and Projected3DCoordinate.ts

---
+++ src/coord/Projected2DCoordinate.ts
@@ -0,0 +1,25 @@
+import { Projected2DSystem } from '../system/Projected2DSystem';
+import { Easting } from '../value/Easting';
+import { Northing } from '../value/Northing';
+import { Coordinate } from './Coordinate';
+
+
+export class Projected2DCoordinate extends Coordinate {
+  public constructor (
+    easting: number | Easting, northing: number | Northing,
+    system: Projected2DSystem = new Projected2DSystem()
+  ) {
+    super( { system, values: [
+      easting instanceof Easting ? easting : new Easting( easting ),
+      northing instanceof Northing ? northing : new Northing( northing )
+    ] } );
+  }
+
+  public get easting () : Easting {
+    return this.values[ 0 ] as Easting;
+  }
+
+  public get northing () : Northing {
+    return this.values[ 1 ] as Northing;
+  }
+}
