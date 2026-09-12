4758913ccc4ad7b6d5e41df8fb86846c812d2652
komed3 <webmaster@komed3.de>
2026-09-13 01:25:40 +0200

    test: create value.test.ts

---
+++ test/value.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, it } from 'vitest';
+import { LongitudeAxis } from '../src/axis';
+import { Value } from '../src/value/Value';
+
+
+const axis = new LongitudeAxis();
+
+
+describe( 'Value', () => {} );
