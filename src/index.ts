337d96f8e14f5bae0d5dc84abaec88c519bd879d
komed3 <webmaster@komed3.de>
2026-09-12 11:39:21 +0200

    update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as axis from './axis';
 import * as dms from './dms';
+import * as ellipsoid from './ellipsoid';
 import * as primem from './primem';
 import * as range from './range';
 import * as unit from './unit';
@@ -7,12 +8,18 @@ import * as value from './value';
 
 export * from './axis';
 export * from './dms';
+export * from './ellipsoid';
 export * from './primem';
 export * from './range';
 export * from './unit';
 export * from './value';
 
-export { axis, dms, primem, range, unit, value };
+export {
+  axis, dms, ellipsoid, primem, range, unit, value
+};
 
-export const geolib = { ...axis, ...dms, ...primem, ...range, ...unit, ...value } as const;
+export const geolib = {
+  ...axis, ...dms, ...ellipsoid, ...primem, ...range, ...unit, ...value
+} as const;
+
 export default geolib;
